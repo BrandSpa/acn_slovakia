5ef79c29c37796a9ea95094734dc2c0f6a9fb9dc
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-10 16:03:42 -0500

    add client dependencies

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+ module.exports = {
+    watch: true,
+     entry: {
+       app: './client/app.js',
+      //  admin: './client/admin/app.js'
+     },
+     output: {
+         path: './public/js',
+         filename: '[name].js'
+     },
+     module: {
+         loaders: [{
+             test: /\.js$/,
+             exclude: /node_modules/,
+             loader: 'babel-loader'
+         }]
+     }
+ }
