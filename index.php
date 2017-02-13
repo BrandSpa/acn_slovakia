d90ed8f06b7878645113e1792f7d07c06c02841c
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-13 10:15:33 -0500

    fix grid and add breackpoints

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php get_header() ?>
 
-<div id="acn_int" class="container" >
+<div id="acn_int" class="l-wrap" >
   <? echo get_user_location() ?>
 <div class="contact-form" data-props='{"country": "Germany"}'></div>
 <div class="contact-form"></div>
 <div class="contact-form"></div>
-
-<div class="col-1-5 col-12-l">
-  <div style="background: red">nea1</div>
+<div class="row">
+  <div class="col-12-l col-2-m col-3">
+    <div style="background: red">nea1</div>
+  </div>
+  <div class="col-12-l col-2-m col-3">
+    <div style="background: red">nea2</div>
+  </div>
+  <div class="col-12-l col-2-m col-3">
+    <div style="background: red">nea3</div>
+  </div>
+  <div class="col-12-l col-2-m col-3">
+    <div style="background: red">nea4</div>
+  </div>
+  <div class="col-12-l col-2-m col-3">
+    <div style="background: red">nea5</div>
+  </div>
 </div>
-<div class="col-1-5 col-12-l">
-  <div style="background: red">nea2</div>
-</div>
-<div class="col-1-5 col-12-l">
-  <div style="background: red">nea3</div>
-</div>
-<div class="col-1-5 col-12-l">
-  <div style="background: red">nea4</div>
-</div>
-<div class="col-1-5 col-12-l">
-  <div style="background: red">nea5</div>
-</div>
-
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
     <?php the_content() ?>
