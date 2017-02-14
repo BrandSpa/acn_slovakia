6db7c97fb239a6c340a585cde05152020401b0c4
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-14 18:18:10 -0500

    get offsetLeft

--- client/components/projects.js
+++ client/components/projects.js
@@ -22,7 +22,7 @@ const Projects = React.createClass({
 
 	changeContent(num) {
 		this.setState({bg: backgroundColors[num]});
-		console.log(this.refs.projectIcons);
+		console.log(this.refs.projectIcons.offsetLeft);
 		let offs = $(`.projects__icons li:nth-child(${num})`).offset();
 		$('.projects__arrow').css({left: `${offs.left}px`});
 	},
