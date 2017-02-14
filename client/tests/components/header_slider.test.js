61aec25e343e30570fe106154c4657a9649afe77
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-14 15:39:13 -0500

    add test to header slider

--- client/tests/components/header_slider.test.js
+++ client/tests/components/header_slider.test.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
-import ContactForm from "../../components/header_slider";
+import HeaderSlider from "../../components/header_slider";
 
 describe("header slider component", () => {
-	it('should change slide', () => {
-		
+	it('should change slide to next', () => {
+		let wrapper = shallow(<HeaderSlider slides={[{title: 'acn pope'}, {title: 'acn pope'}, {title: 'acn pope'}]} />);
+		wrapper.instance().nextSlide();
+		wrapper.instance().nextSlide();
+		expect(wrapper.state()).toEqual({currentSlide: 2, left: '-200%'});
 	})
+
+	it('should change slide to prev', () => {
+		let wrapper = shallow(<HeaderSlider slides={[{title: 'acn pope'}, {title: 'acn pope'}, {title: 'acn pope'}]} />);
+		wrapper.instance().nextSlide();
+		wrapper.instance().nextSlide();
+		wrapper.instance().prevSlide();
+		expect(wrapper.state()).toEqual({currentSlide: 1, left: '-100%'});
+	})
+
 });
