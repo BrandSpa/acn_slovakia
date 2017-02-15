08952c649dda61344c844e583f2e10fc12b6303d
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-14 23:12:29 -0500

    clean app

--- client/app.js
+++ client/app.js
@@ -1,6 +1,4 @@
 "use strict";
-import React from "react";
-import { render } from "react-dom";
 import multipleRender from "./lib/mutiple_render";
 import ContactForm from "./components/contact_form";
 import HeaderSlider from "./components/header_slider";
@@ -9,6 +7,3 @@ import Projects from './components/projects';
 multipleRender(".contact-form", ContactForm);
 multipleRender(".header-slider", HeaderSlider);
 multipleRender(".projects-container", Projects);
-
-
-
