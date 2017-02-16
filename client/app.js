2e61b10151a1cfeab71b8d594b002a83249b0af3
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-16 18:45:30 -0500

    correct name has class

--- client/app.js
+++ client/app.js
@@ -21,10 +21,11 @@ let langs = $('.lang-item').not( $(".current-lang") );
 
 currentLang.on('click', (e) => {
 	e.preventDefault();
-	if($('.dropdown-content').hasClas('dropdown-content--show')) {
+	if($('.dropdown-content').hasClass('dropdown-content--show')) {
 		$('.dropdown-content').removeClass('dropdown-content--show');
 		return;
 	}
+	
 	$('.dropdown-content').addClass('dropdown-content--show');
 });
 
--- public/js/app.js
+++ public/js/app.js
@@ -31295,10 +31295,11 @@ var langs = $('.lang-item').not($(".current-lang"));
 
 currentLang.on('click', function (e) {
 	e.preventDefault();
-	if ($('.dropdown-content').hasClas('dropdown-content--show')) {
+	if ($('.dropdown-content').hasClass('dropdown-content--show')) {
 		$('.dropdown-content').removeClass('dropdown-content--show');
 		return;
 	}
+
 	$('.dropdown-content').addClass('dropdown-content--show');
 });
 
