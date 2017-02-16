aab38b68bf26b57ec13a2bdeee8a708813b8a1dd
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-16 16:45:00 -0500

    h5 to 18 and adding posts component

---
+++ client/components/posts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import qs from 'qs';
+import request from 'axios';
+import Isotipe from 'isotipe';
+
+const Posts = React.createClass({
+	render() {
+
+	}
+});
+
+export default Posts;
