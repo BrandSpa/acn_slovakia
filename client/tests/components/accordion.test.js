55c4f9652106ff2fdf7c4a73c53b142a652c463e
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-16 15:29:16 -0500

    add accordion test

---
+++ client/tests/components/accordion.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Accordion from "../../components/accordion";
+
+describe('accordion component', () => {
+	it('it should toggle to true', () => {
+		let wrapper = shallow(<Accordion />);
+		wrapper.find('button').simulate('click');
+		expect(wrapper.state().show).toBe(true);
+	})
+
+	it('it should have icon up', () => {
+			let wrapper = shallow(<Accordion />);
+		wrapper.find('button').simulate('click');
+		expect(wrapper.find('button i').hasClass('ion-chevron-up')).toBe(true);
+	})
+
+	it('it should toggle to false', () => {
+		let wrapper = shallow(<Accordion />);
+		wrapper.find('button').simulate('click');
+		wrapper.find('button').simulate('click');
+		expect(wrapper.state().show).toBe(false);
+	})
+
+	it('it should have icon down', () => {
+			let wrapper = shallow(<Accordion />);
+		wrapper.find('button').simulate('click');
+		wrapper.find('button').simulate('click');
+		expect(wrapper.find('button i').hasClass('ion-chevron-down')).toBe(true);
+	})
+})
