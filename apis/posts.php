d3e0599d4329896c547324e92c06f5f513161448
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-16 16:34:45 -0500

    add paged

--- apis/posts.php
+++ apis/posts.php
@@ -1,8 +1,4 @@
 <?php
-function addImageToPost($post) {
-	$post['image'] = get_the_post_thumbnail_url($post->ID, 'full');
-	return $post;
-}
 
 function bs_get_posts($type = 'post', $paged = 1, $category = '') {
 	$query = new Wp_Query(array(
