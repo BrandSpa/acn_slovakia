db863c4d98b8e0de6c66df2cab36407d6eb626d4
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-17 13:53:28 -0500

    add visual compose map donate

--- shortcodes/donate.php
+++ shortcodes/donate.php
@@ -39,3 +39,50 @@ return ob_get_clean();
 }
 
 add_shortcode('bs_donate_react', 'bs_donate_react_sc');
+
+add_action( 'vc_before_init', 'bs_donate_react_vc' );
+
+  function bs_donate_react_vc() {
+		$params = [];
+    $atts = [
+      "other" => "Other",
+		 "monthly" => "Monthly",
+		 "once" => "Once",
+		 "placeholder_amount" => "Amount",
+    "placeholder_credit_card" => "Credit Card Number",
+    "placeholder_month" => "MM",
+    "placeholder_year" => "YY",
+    "placeholder_cvc" => "CVC",
+    "placeholder_name" => "Name",
+    "placeholder_email" => "Email",
+    "placeholder_country" => "Country",
+		"validation_declined" => "Your transaction was not accepted, try again",
+    "validation_card" => "Incorrect card",
+    "validation_month" => "Incorrect month",
+    "validation_year" => "Incorrect year",
+    "validation_cvc" => "Incorrect cvc",
+    "validation_name" => "Incorrect name",
+    "validation_email" => "Incorrect email",
+    "validation_country" => "Incorrect country",
+    ];
+    
+    foreach($atts as $key => $val) {
+      array_push($params, [
+         "type" => "textfield",
+         "heading" =>  str_replace('_', ' ', $key),
+         "param_name" => $key,
+         "value" => $val
+
+      ]);
+    }
+
+  	vc_map(
+      array(
+        "name" =>  "BS Donate",
+        "base" => "bs_donate_react",
+        "category" =>  "BS",
+        "params" => $params
+      ) 
+    );
+  }
+
