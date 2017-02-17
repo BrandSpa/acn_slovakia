3364ea18ee529b651fcc6e722f9bf4b9e4c1e557
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-16 21:48:22 -0500

    add getCountry and donate sc

--- client/app.js
+++ client/app.js
@@ -6,6 +6,7 @@ import sectionVideo from './components/section_video';
 import Projects from './components/projects';
 import Accordion from './components/accordion';
 import Posts from './components/posts';
+import Donate from './components/donate';
 
 multipleRender(".contact-form", ContactForm);
 multipleRender(".header-slider", HeaderSlider);
@@ -13,26 +14,32 @@ multipleRender(".section-video", sectionVideo);
 multipleRender(".projects-container", Projects);
 multipleRender(".bs-accordion", Accordion);
 multipleRender(".bs-posts", Posts);
+multipleRender(".bs-posts", Donate);
 
-const currentLang = $('.current-lang > a');
-$('.current-lang').addClass('dropdown');
-$('.current-lang').append('<div class="dropdown-content"></div>');
-let langs = $('.lang-item').not( $(".current-lang") );
+function setMenu() {
+	const currentLang = $('.current-lang > a');
+	$('.current-lang').addClass('dropdown');
+	$('.current-lang').append('<div class="dropdown-content"></div>');
+	let langs = $('.lang-item').not( $(".current-lang") );
 
-currentLang.on('click', (e) => {
-	e.preventDefault();
-	if($('.dropdown-content').hasClass('dropdown-content--show')) {
-		$('.dropdown-content').removeClass('dropdown-content--show');
-		return;
-	}
-	
-	$('.dropdown-content').addClass('dropdown-content--show');
-});
+	currentLang.on('click', (e) => {
+		e.preventDefault();
+		if($('.dropdown-content').hasClass('dropdown-content--show')) {
+			$('.dropdown-content').removeClass('dropdown-content--show');
+			return;
+		}
+		
+		$('.dropdown-content').addClass('dropdown-content--show');
+	});
 
-langs.each(function() {
-	$('.dropdown-content').append($(this).html());
-	$(this).remove();
-});
+	langs.each(function() {
+		$('.dropdown-content').append($(this).html());
+		$(this).remove();
+	});
 
-const newText = `${currentLang.text()} <i class="ion-chevron-down"></i>`;
-currentLang.html(newText);
+	const newText = `${currentLang.text()} <i class="ion-chevron-down"></i>`;
+	currentLang.html(newText);
+
+}
+
+setMenu();
---
+++ shortcodes/donate.php
@@ -0,0 +1,41 @@
+<?php
+
+function bs_donate_react_sc($atts, $content = null) {
+	 $at = shortcode_atts([
+		 "country" => getCountry(),
+		 "other" => "Other",
+		 "monthly" => "Monthly",
+		 "once" => "Once",
+		 "placeholder_amount" => "Amount",
+    "placeholder_credit_card" => "Credit Card Number",
+    "placeholder_month" => "MM",
+    "placeholder_year" => "YY",
+    "placeholder_cvc" => "CVC",
+    "placeholder_name" => "Name",
+    "placeholder_email" => "Email",
+    "placeholder_country" => "Country",
+		"validation_declined" => "Your transaction was not accepted, try again",
+    "validation_card" => "Incorrect card",
+    "validation_month" => "Incorrect month",
+    "validation_year" => "Incorrect year",
+    "validation_cvc" => "Incorrect cvc",
+    "validation_name" => "Incorrect name",
+    "validation_email" => "Incorrect email",
+    "validation_country" => "Incorrect country",
+		"template_uri" => str_replace("http:", "", get_template_directory_uri())
+	 ], $atts);
+
+	ob_start();
+?>
+
+<div 
+	id="bs-donate-react" 
+	data-props='<?php echo json_encode($at) ?>'
+>
+</div>
+
+<?php
+return ob_get_clean();
+}
+
+add_shortcode('bs_donate_react', 'bs_donate_react_sc');
