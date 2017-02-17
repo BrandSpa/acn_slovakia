802f80f252cef789070e958b97a543e7435f1916
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-17 16:05:32 -0500

    reload

--- public/js/app.js
+++ public/js/app.js
@@ -13499,6 +13499,7 @@ var Posts = _react2.default.createClass({
 	componentDidUpdate: function componentDidUpdate() {
 		if (this.state.posts && this.state.posts.length > 0 && this.iso) {
 			console.log('reload');
+			this.iso.reloadItems();
 			this.iso.layout();
 		} else if (this.state.posts && this.state.posts.length > 0) {
 			this.initIsotope();
