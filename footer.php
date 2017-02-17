552a13ecf7a48d2ec6a53f506e03d0e340b061e7
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-17 11:11:20 -0500

    add stripe lib

--- footer.php
+++ footer.php
@@ -12,6 +12,7 @@
   });
 </script>
 
-<script src="<?php echo get_template_directory_uri() ?>/public/js/app.js"></script>
+<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/public/js/app.js"></script>
 </body>
 </html>
