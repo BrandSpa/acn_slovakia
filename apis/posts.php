8c4251648c1649ddc518d7cbbb1d07d212eef9e7
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-17 11:59:25 -0500

    get post short api

--- apis/posts.php
+++ apis/posts.php
@@ -9,6 +9,7 @@ function bs_get_posts($type = 'post', $paged = 1, $category = '') {
 	
 	$posts = array_map(function($post) {
 		$post->post_image = get_the_post_thumbnail_url($post->ID, 'full');
+		$post->post_short = wp_strip_all_tags(substr($post->post_content, 0, 150));
 		return $post;
 	}, $query->get_posts());
 	
