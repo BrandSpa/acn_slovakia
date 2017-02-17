de47995f1a1b98e3ae0c671bdf4885d39da877d6
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-16 23:42:55 -0500

    move nav to template

--- header.php
+++ header.php
@@ -12,23 +12,4 @@
 </head>
 <body>
 
-<div class="nav">
-	  <a href="#" class="logo">
-        <?php if( !empty( get_option("logo") ) ): ?>
-          <img src='<?php echo str_replace('http:', '', get_option("logo")) ?>'  alt="acn logo" class="img-responsive" width="150"  />
-        <?php endif; ?>
-     </a>
-
-	<ul class="menu">
-	  <?php
-			$args = array(
-            'theme_location' => 'header',
-            'container' => false,
-            'echo' => false
-			);
-
-			$menu = wp_nav_menu( $args);
-    	echo clean_menu($menu);
-    ?>
-	</ul>
-</div>
+<?php require('templates/nav.php') ?>
--- templates/nav.php
+++ templates/nav.php
@@ -1,5 +1,20 @@
-<div id="nav">
-<div class="logo">
-	<a href="#"><img src="" alt=""></a>
-</div>
+<div class="nav">
+	  <a href="#" class="logo">
+        <?php if( !empty( get_option("logo") ) ): ?>
+          <img src='<?php echo str_replace('http:', '', get_option("logo")) ?>'  alt="acn logo" class="img-responsive" width="150"  />
+        <?php endif; ?>
+     </a>
+
+	<ul class="menu">
+	  <?php
+			$args = array(
+            'theme_location' => 'header',
+            'container' => false,
+            'echo' => false
+			);
+
+			$menu = wp_nav_menu( $args);
+    	echo clean_menu($menu);
+    ?>
+	</ul>
 </div>
