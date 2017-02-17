fc5e48dbb0adc7b0b187d3aa1c26578caa7d5b8f
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-16 22:45:51 -0500

    get user location

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
      </a>
 
 	<ul class="menu">
-		<li><?php echo get_user_location() ?></li>
+		<li><?php var_dump(get_user_location()) ?></li>
 	  <?php
 			$args = array(
             'theme_location' => 'header',
