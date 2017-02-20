4d60986be84e94cbfbbe36b21c3d3e5f4dfb7dfb
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-20 18:10:15 -0500

    validate if show posts

--- functions.php
+++ functions.php
@@ -43,3 +43,12 @@ require('shortcodes/donate.php');
 require('shortcodes/posts.php');
 require('shortcodes/contact_info.php');
 
+function show_posts() {
+	if(function_exists('pll_current_language')) {
+		$lang = pll_current_language();
+		if($lang == 'es' || $lang == 'en') return true;
+		return false;
+	}
+
+	return false;
+}
