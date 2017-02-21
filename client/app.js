1da0cab1404aaae14c922c92ca1a184622efdeae
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-21 16:53:30 -0500

    add margin left to body on mobile menu open

--- client/app.js
+++ client/app.js
@@ -52,8 +52,11 @@ function setMenuMobile() {
 	$('.open-menu').on('click', () => {
 		
 		if($('.menu--mobile').hasClass('menu--mobile--open')) {
+			$(document.body).css({'margin-left': '200px'});
 			$('.menu--mobile').removeClass('menu--mobile--open');
+
 		} else {
+			$(document.body).css({'margin-left': '0'});
 			$('.menu--mobile').addClass('menu--mobile--open');
 		}
 
--- public/js/app.js
+++ public/js/app.js
@@ -32915,8 +32915,10 @@ function setMenuMobile() {
 	$('.open-menu').on('click', function () {
 
 		if ($('.menu--mobile').hasClass('menu--mobile--open')) {
+			$(document.body).css({ 'margin-left': '200px' });
 			$('.menu--mobile').removeClass('menu--mobile--open');
 		} else {
+			$(document.body).css({ 'margin-left': '0' });
 			$('.menu--mobile').addClass('menu--mobile--open');
 		}
 	});
