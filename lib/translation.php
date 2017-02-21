5633da6a07b79ac93e7d8e10ac57c3ad85cee62b
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-20 21:16:19 -0500

    add text

--- lib/translation.php
+++ lib/translation.php
@@ -15,7 +15,8 @@ function register_translation($name ='', $text, $group = 'BS', $multiline = true
 
 $translations = [
 	'“I invite you all, together with ACN, to do everywhere in the world, a work of mercy.”',
-	'Founded in 1947 as a Catholic aid organization for war refugees and recognized as a papal foundation since 2011, ACN is dedicated to the service of Christians around the world, through information, prayer and action, wherever they are persecuted or oppressed or suffering material need. ACN supports every year an average of 6000 projects in close to 150 countries, thanks to private donations, as the foundation receives no public funding.'
+	'Founded in 1947 as a Catholic aid organization for war refugees and recognized as a papal foundation since 2011, ACN is dedicated to the service of Christians around the world, through information, prayer and action, wherever they are persecuted or oppressed or suffering material need. ACN supports every year an average of 6000 projects in close to 150 countries, thanks to private donations, as the foundation receives no public funding.',
+	'ACN – Aid to the Church in Need gGmbH, HRB 8446 is non-profit organization officially registered in Germany and audited internationally by KPMG.'
 ];
 
 foreach($translations as $key => $trans) {
