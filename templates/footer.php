3e14fa71afcbc09167a697a10bd39810492e382f
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-20 19:18:46 -0500

    add more structure to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,27 @@
 <div class="bs-footer">
 	<div class="bs-footer__left col-3-l">
 		<img width="86" height="86" src="//acninternational.org/wp-content/uploads/2017/02/pope.png" class="vc_single_image-img attachment-medium" alt="pope">
+			<?php echo gett('“I invite you all, together with ACN, to do everywhere in the world, a work of mercy.”'); ?>
 	</div>
 	<div class="bs-footer__right col-9-l">
-		<img width="86" height="86" src="//acninternational.org/wp-content/uploads/2017/02/pope.png" class="vc_single_image-img attachment-medium" alt="pope">
-		<?php echo gett('“I invite you all, together with ACN, to do everywhere in the world, a work of mercy.”'); ?>
+		<div class="bs-footer__right--top">
+			<ul class="bs-footer__menu">
+				<?php
+					$args = array(
+						'theme_location' => 'footer',
+						'container' => false,
+						'echo' => false
+					);
+
+					$menu = wp_nav_menu( $args);
+					echo clean_menu($menu);
+				?>
+			</ul>
+		</div>
+		<div class="bs-footer__right--bottom">
+
+
+		</div>
 	</div>
 	<div class="bs-footer__bottom--top">
 	
