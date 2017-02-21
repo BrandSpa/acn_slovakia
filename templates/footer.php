e7c9f9abb6efbc906e5ef4dee7caffc9888d87f9
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-20 20:53:45 -0500

    add more structure

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,8 @@
 <div class="bs-footer">
 	<div class="bs-footer__left col-3-l">
 		<img width="86" height="86" src="//acninternational.org/wp-content/uploads/2017/02/pope.png" class="vc_single_image-img attachment-medium" alt="pope">
-			<?php echo gett('“I invite you all, together with ACN, to do everywhere in the world, a work of mercy.”'); ?>
+			<p><?php echo gett('“I invite you all, together with ACN, to do everywhere in the world, a work of mercy.”'); ?></p>
+			<img width="122" height="23" src="//acninternational.org/wp-content/uploads/2017/02/SignPope.png" class="vc_single_image-img attachment-thumbnail" alt="signpope">
 	</div>
 	<div class="bs-footer__right col-9-l">
 		<div class="bs-footer__right--top">
