f893a1941c4724562de79b825027cb440f737c6e
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-20 21:33:22 -0500

    add paddings

--- templates/footer.php
+++ templates/footer.php
@@ -3,10 +3,10 @@
 </div>
 
 <div class="bs-about">
-	<div class="col-3-m">
+	<div class="col-3-l bs-about__left">
 		<img width="76" height="103" src="//acninternational.org/wp-content/uploads/2016/11/logoACNwhy2.png" class="vc_single_image-img attachment-full" alt="logoacnwhy2">
 	</div>
-	<div class="col-9-m">
+	<div class="col-9-l bs-about__right">
 		<h3><?php echo gett('ABOUT US') ?></h3>
 		<p><?php echo gett('Founded in 1947 as a Catholic aid organization for war refugees and recognized as a papal foundation since 2011, ACN is dedicated to the service of Christians around the world, through information, prayer and action, wherever they are persecuted or oppressed or suffering material need. ACN supports every year an average of 6000 projects in close to 150 countries, thanks to private donations, as the foundation receives no public funding.') ?></p>
 	</div>
