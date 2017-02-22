3c2aeb7ca105a62869eed5b0c76260e61dc0cfd6
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-21 19:01:03 -0500

    doc tag manager

--- header.php
+++ header.php
@@ -27,13 +27,14 @@
 <body>
 <?php if(get_option('gta_id')): ?>
 <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo get_option('gta_id') ?>"
+  <!--<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo get_option('gta_id') ?>"
   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','<?php echo get_option('gta_id') ?>');</script>
+  })(window,document,'script','dataLayer','<?php echo get_option('gta_id') ?>');</script>-->
   <!-- End Google Tag Manager -->
 
 <?php endif; ?>
