478612173d9f8a99cbac4f8cc6650e8a73f5e368
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-23 11:45:48 -0500

    add post header image

--- single.php
+++ single.php
@@ -2,6 +2,7 @@
 
 <div id="acn_int" class="bs-post l-wrap" >
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<?php require('templates/post_header_image.php') ?>
 	<?php if(get_the_post_thumbnail_url($post->ID, 'full')): ?>
 <?php else: ?>
 	<div class="bs-post__header col-12-l">
@@ -15,11 +16,8 @@
 	 	<?php the_content() ?>
 	</div>
    
-    
   <?php endwhile; else : ?>
-    <h1>
-    <?php echo gett('404') ?>
-    </h1>
+    <h2> <?php echo gett('404') ?> </h2>
   <?php endif; ?>
 </div>
 
