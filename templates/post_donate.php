f12d35c6e074d9661fb7ab74089b8d28d59bcd7d
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-23 14:58:31 -0500

    add style to post donate

--- templates/post_donate.php
+++ templates/post_donate.php
@@ -1,10 +1,9 @@
-<div class="bs-post__donate">
+<div class="bs-post__donate" >
 	<div class="l-wrap">
 		<div class="col-8-l">
-			<h2>My gift to support the ACN mission with the persecuted Christians and those in need.</h2>
+			<h3>My gift to support the ACN mission with the persecuted Christians and those in need.</h3>
 			<?php echo do_shortcode('[bs_donate_react]') ?>
 		</div>
-		
 	</div>
 </div>
 
