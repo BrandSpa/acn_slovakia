928bf7d5381d1e24c05f11f6804fb336dea8984f
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-23 11:25:02 -0500

    add cols

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
 	<?php if(get_the_post_thumbnail_url($post->ID, 'full')): ?>
 <?php else: ?>
 	<div class="bs-post__header col-12-l">
-		<div class="bs-post__header__title">
+		<div class="bs-post__header__title col-10-l col-12-s">
 			<h2><?php the_title() ?></h2>
 		</div>
 	</div>
