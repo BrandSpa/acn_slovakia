3b42bf0084b1f765557dddee24134b12aaed8c6f
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-23 11:49:08 -0500

    fix class

--- templates/post_header_image.php
+++ templates/post_header_image.php
@@ -3,7 +3,7 @@
 ?>
 
 	<div class="bs-post__header--image" style="<?php echo $style ?>">
-		<div class="bs-post__header__title">
+		<div class="bs-post__header--image__title">
 			<h2><?php the_title() ?></h2>
 		</div>
 	</div>
