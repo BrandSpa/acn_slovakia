1140114255904b088f8a8af51ba8892801126fa1
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-23 11:09:40 -0500

    change styles

--- single.php
+++ single.php
@@ -2,21 +2,16 @@
 
 <div id="acn_int" class="bs-post" >
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<?php
-
-	if(get_the_post_thumbnail_url($post->ID, 'full')) {
-		$style = "background-image: url(" . get_the_post_thumbnail_url($post->ID, 'full') . "); background-size: cover; background-position: center; min-height: 400px"; 
-	} else {
+	<?php if(get_the_post_thumbnail_url($post->ID, 'full')): ?>
+<?php else: ?>
 		$style = "background: #F4F4F4";
-	}
-
-	?>
 	<div class="bs-post__header" style="<?php echo $style ?>">
-		<div class="bs-post__title">
+		<div class="bs-post__header__title">
 			<h2><?php the_title() ?></h2>
 		</div>
 	</div>
-		
+<?php endif; ?>
+
 	<div class="bs-post__content">
 	 	<?php the_content() ?>
 	</div>
---
+++ templates/post_header_image.php
@@ -0,0 +1,10 @@
+<?php 
+	$style = "background-image: url(" . get_the_post_thumbnail_url($post->ID, 'full') . "); background-size: cover; background-position: center; min-height: 400px"; 
+?>
+
+	<div class="bs-post__header--image" style="<?php echo $style ?>">
+		<div class="bs-post__header__title">
+			<h2><?php the_title() ?></h2>
+		</div>
+	</div>
+		
