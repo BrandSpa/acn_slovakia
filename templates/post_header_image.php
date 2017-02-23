2612952ded4420d08d44c990dec199d3e02d100a
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-23 12:09:58 -0500

    add more styles

--- templates/post_header_image.php
+++ templates/post_header_image.php
@@ -1,5 +1,5 @@
 <?php 
-	$style = "background-image: url(" . get_the_post_thumbnail_url($post->ID, 'full') . "); background-size: cover; background-position: center; min-height: 400px"; 
+	$style = "background-image: url(" . get_the_post_thumbnail_url($post->ID, 'full') . ");"; 
 ?>
 
 	<div class="bs-post__header--image" style="<?php echo $style ?>">
