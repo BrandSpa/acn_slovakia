2c4092584d48e9d8fe1b497d7fe70f16d9462e4c
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-23 00:09:14 -0500

    change size text posts api

--- apis/posts.php
+++ apis/posts.php
@@ -9,7 +9,7 @@ function bs_get_posts($type = 'post', $paged = 1, $category = '') {
 	
 	$posts = array_map(function($post) {
 		$post->post_image = get_the_post_thumbnail_url($post->ID, 'full');
-		$post->post_short = wp_strip_all_tags(substr($post->post_content, 0, 300));
+		$post->post_short = wp_strip_all_tags(substr($post->post_content, 0, 120));
 		$post->post_permalink = get_post_permalink($post->ID);
 		return $post;
 	}, $query->get_posts());
