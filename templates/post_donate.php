20c2894dd89ad838fa78db3ae40a1eeb3cc3e7b4
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-23 15:07:14 -0500

    add cols

--- templates/post_donate.php
+++ templates/post_donate.php
@@ -1,8 +1,12 @@
 <div class="bs-post__donate" >
 	<div class="l-wrap">
 		<div class="col-8-l">
-			<h3>My gift to support the ACN mission with the persecuted Christians and those in need.</h3>
-			<?php echo do_shortcode('[bs_donate_react]') ?>
+				<div class="col-3-l">
+				<h3>My gift to support the ACN mission with the persecuted Christians and those in need.</h3>
+			</div>
+			<div class="col-4-l">
+				<?php echo do_shortcode('[bs_donate_react]') ?>
+			</div>
 		</div>
 	</div>
 </div>
