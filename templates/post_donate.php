acc74d790e8ab6f504f6e9b42fc2e44f0a62bab1
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-23 15:28:39 -0500

    float none

--- templates/post_donate.php
+++ templates/post_donate.php
@@ -1,10 +1,10 @@
 <div class="bs-post__donate" >
 	<div class="l-wrap">
-		<div class="col-8-l">
+		<div class="col-8-l" style="float:none; margin: 0 auto">
 				<div class="col-3-l">
 				<h3>My gift to support the ACN mission with the persecuted Christians and those in need.</h3>
 			</div>
-			<div class="col-4-l">
+			<div class="col-8-l">
 				<?php echo do_shortcode('[bs_donate_react]') ?>
 			</div>
 		</div>
