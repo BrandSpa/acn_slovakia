b45d5eacb68bfabb029f2fd54fbe3a586596aa6d
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-23 14:57:19 -0500

    add more struture to post donate

--- templates/post_donate.php
+++ templates/post_donate.php
@@ -1,6 +1,10 @@
 <div class="bs-post__donate">
 	<div class="l-wrap">
-		<?php echo do_shortcode('[bs_donate_react]') ?>
+		<div class="col-8-l">
+			<h2>My gift to support the ACN mission with the persecuted Christians and those in need.</h2>
+			<?php echo do_shortcode('[bs_donate_react]') ?>
+		</div>
+		
 	</div>
 </div>
 
