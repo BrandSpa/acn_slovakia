07b8fb8041fb1891fef46086601f78826b96ed43
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-23 15:30:37 -0500

    change cols

--- templates/post_donate.php
+++ templates/post_donate.php
@@ -1,7 +1,7 @@
 <div class="bs-post__donate" >
 	<div class="l-wrap">
 		<div class="col-8-l" style="float:none; margin: 0 auto; padding: 0">
-				<div class="col-3-l">
+				<div class="col-4-l">
 				<h3>My gift to support the ACN mission with the persecuted Christians and those in need.</h3>
 			</div>
 			<div class="col-8-l">
