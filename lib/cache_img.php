dd3a6d565bfd8d1757d5c61e1c8d2b65215c0bab
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-25 01:55:18 -0500

    check first vendor autoload

--- lib/cache_img.php
+++ lib/cache_img.php
@@ -1,9 +1,14 @@
 <?php
 $dir_base =  str_replace('lib', '', __DIR__);
-require $dir_base . '/vendor/autoload.php';
+if( file_exists($dir_base . '/vendor/autoload.php') ) {
 
-function getCacheImg($filename = '') {
-	$imagecache = new ImageCache();
-	$imagecache->cached_image_directory = $dir_base . '/public/img/cached';
-	$cached_src_one = $imagecache->cache( $dir_base . '/public/img/visa.png' );
+  require $dir_base . 'vendor/autoload.php';
+
+
+	function getCacheImg($filename = '') {
+		$imagecache = new ImageCache();
+		$imagecache->cached_image_directory = $dir_base . '/public/img/cached';
+		$cached_src_one = $imagecache->cache( $dir_base . '/public/img/visa.png' );
+	}
+
 }
