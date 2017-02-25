8da2681f7d353027c731a85a06e1c4126fcaa15a
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-24 23:48:33 -0500

    add jquery via wp

--- footer.php
+++ footer.php
@@ -1,7 +1,5 @@
 	<?php require('templates/footer.php') ?>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
 	<!--wordpress files-->
 	  <?php wp_footer() ?>
 	<!-- /wordpress files-->
