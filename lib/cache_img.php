7c025e5cb287cddfa7de7ee76ead5a02b3862e0f
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-25 01:50:49 -0500

    clean param

--- lib/cache_img.php
+++ lib/cache_img.php
@@ -2,9 +2,8 @@
 $dir_base =  str_replace('lib', '', __DIR__);
 require $dir_base . '/vendor/autoload.php';
 
-function getCacheImg($filename) {
+function getCacheImg($filename = '') {
 	$imagecache = new ImageCache();
 	$imagecache->cached_image_directory = $dir_base . '/public/img/cached';
 	$cached_src_one = $imagecache->cache( $dir_base . '/public/img/visa.png' );
 }
-
