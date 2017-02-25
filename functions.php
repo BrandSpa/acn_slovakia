9c63bd79e140e297cf6ba83662811e1380d0e667
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-24 23:52:59 -0500

    mmm

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ function modify_jquery() {
 	if (!is_admin()) {
 		// comment out the next two lines to load the local copy of jQuery
 		wp_deregister_script('jquery');
-		wp_deregister_script( 'wp-embed' );
+		// wp_deregister_script( 'wp-embed' );
 		wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', false, '3.1.1', true); 
 		wp_enqueue_script('jquery');
 	}
