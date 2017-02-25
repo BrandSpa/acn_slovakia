1e38703b1a871e3adec6761846446d526e35df63
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-24 23:38:50 -0500

    up jquery

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 	<?php require('templates/footer.php') ?>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
 	<!--wordpress files-->
 	  <?php wp_footer() ?>
 	<!-- /wordpress files-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
 	<!--async load app-->
 <script type="text/javascript">
   function downloadJSAtOnload() {
