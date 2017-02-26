a8e1ed9fbd9f91110c94226ee2a18065dcacca85
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-26 12:49:50 -0500

    change jquery

--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@ function modify_jquery() {
 		// comment out the next two lines to load the local copy of jQuery
 		wp_deregister_script( 'wp-embed' );
 		wp_deregister_script('jquery');
-		wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', false, '3.1.1', true); 
+		wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.slim.min.js', false, '3.1.1', true); 
 		wp_enqueue_script('jquery');
 	}
 }
