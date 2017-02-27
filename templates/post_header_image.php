e0a57d28d7ae51b88b7a9b65697c85b096b128d1
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-27 18:31:14 -0500

    change bg

--- templates/post_header_image.php
+++ templates/post_header_image.php
@@ -1,7 +1,10 @@
-
+<div class="l-wrap">
+	
 	<div class="bs-post__header--image">
-	<div class="bs-post__header--image__bg" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>)"></div>
+	<div class="bs-post__header--image__bg" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>)">
 		<div class="bs-post__header--image__title">
 				<h2><?php the_title() ?></h2>
 			</div>
+		</div>
 	</div>
+</div>
