6b9b1b8606349fac8300b6fddfdad854ee182e6b
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-27 18:29:07 -0500

    change to 40

--- templates/post_header_image.php
+++ templates/post_header_image.php
@@ -2,6 +2,6 @@
 	<div class="bs-post__header--image">
 	<div class="bs-post__header--image__bg" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>)"></div>
 		<div class="bs-post__header--image__title">
-				<h3><?php the_title() ?></h3>
+				<h2><?php the_title() ?></h2>
 			</div>
 	</div>
