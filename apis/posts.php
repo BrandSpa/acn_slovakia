83ec6fc05c284e4895e1abdbf1e7dcb47cb02619
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-28 14:47:06 -0500

    preg replace to remove shortcoes

--- apis/posts.php
+++ apis/posts.php
@@ -10,7 +10,7 @@ function bs_get_posts($type = 'post', $paged = 1, $category = '') {
 	$posts = array_map(function($post) {
 		$thumbId = get_post_thumbnail_id($post->ID);
 		$post->post_image = str_replace('http:', '', wp_get_attachment_url($thumbId));
-		$post->post_short = wp_strip_all_tags(substr($post->post_content, 0, 200));
+		$post->post_short = preg_replace('/\[(.*?)\]/', '', wp_strip_all_tags(substr($post->post_content, 0, 200)) );
 		$post->post_permalink = get_post_permalink($post->ID);
 		return $post;
 	}, $query->get_posts());
