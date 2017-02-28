b0232e947e31d61cd8774e6d4d14557fd7beed24
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-28 15:59:25 -0500

    change to link

--- templates/post_header_image.php
+++ templates/post_header_image.php
@@ -1,9 +1,7 @@
-
-	<div class="bs-post__header--image" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>)">\
-
+<div class="bs-post__header--image" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>)">\
 	<div class="bs-post__header--image__title">
-	<h3><?php the_title() ?></h3>
-
-</div>
+		<h3><?php the_title() ?></h3>
+		<a className="arrow-down"><i className="ion-chevron-down"></i></a>
 	</div>
+</div>
 
