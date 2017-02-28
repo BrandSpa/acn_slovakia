e1c6bd5207fecfdffaa12c58d1245f2114ecbaa1
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-28 15:29:07 -0500

    fix class to classname and fix style titles

--- client/components/header_slide.js
+++ client/components/header_slide.js
@@ -35,7 +35,7 @@ const headerSlide = React.createClass({
           <div className="slider__slide__content">
             <h2>{title}</h2>
             <h4>{subtitle}</h4>
-            <div class="arrow-down"><i class="ion-chevron-down"></i></div>
+            <div className="arrow-down"><i className="ion-chevron-down"></i></div>
           </div>
       </div>
     );
