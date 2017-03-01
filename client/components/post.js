b941581a5f5a9fd82d658e4afe84458c4242d352
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-28 21:08:02 -0500

    width 100 if not has image

--- client/components/post.js
+++ client/components/post.js
@@ -7,15 +7,17 @@ const Post = React.createClass({
 
 	render() {
 		const { post, type } = this.props;
+		const img = post.post_image ? <img src={post.post_image} onLoad={this.handleImageLoaded} /> : '';
 
 		return (
 			<div className={type == 'main' ? 'grid-item grid-item--main' : 'grid-item'}>
 					<div className={type == 'main' ? 'grid-item__content grid-item--main__content' : 'grid-item__content'}>
-							{post.post_image ? <img src={post.post_image} onLoad={this.handleImageLoaded} /> : ''}
+							{img}
 							<div
 									className={
 											type == 'main' ? 'grid-item__content__texts grid-item--main__content__texts' : 'grid-item__content__texts'
 									}
+									style={img == '' ? {width: '100%'} : {} }
 							>
 									<h5><a href={post.post_permalink}>{post.post_title}</a></h5>
 									<p>{`${post.post_short}...`}</p>
