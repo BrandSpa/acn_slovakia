8a6d9f8588f507542a1fffb16e4ac8dbc4b54c26
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-28 23:41:08 -0500

    get short title

--- client/components/post.js
+++ client/components/post.js
@@ -4,6 +4,13 @@ const Post = React.createClass({
 	handleImageLoaded() {
   	this.props.onImageLoaded();
   },
+	getDefaultProps() {
+		return {
+			post: {
+				post_title: ''
+			}
+		}
+	},
 
 	render() {
 		const { post, type, read_more } = this.props;
@@ -21,7 +28,7 @@ const Post = React.createClass({
 									}
 									style={img == '' ? {width: '100%'} : {} }
 							>
-									<h5><a href={post.post_permalink}>{post.post_title}</a></h5>
+									<h5><a href={post.post_permalink}>{ post.post_title ? post.post_title.substring(0, 70) + '...' : ''}</a></h5>
 									<p>{`${post.post_short}...`}</p>
 									<span className="grid-item__content__texts__readmore">{read_more}...</span>
 							</div>
