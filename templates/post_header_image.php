6d278c7cdc9ef53da0ed3c1704e1d5b0d8b5bfa6
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-01 00:18:18 -0500

    add links to latest

--- templates/post_header_image.php
+++ templates/post_header_image.php
@@ -1,4 +1,4 @@
-<div class="bs-post__header--image" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>)">Â
+<div class="bs-post__header--image" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>)">
 	<div class="bs-post__header--image__title">
 		<h3><?php the_title() ?></h3>
 		<a class="arrow-down"><i class="ion-chevron-down"></i></a>
--- templates/post_latest.php
+++ templates/post_latest.php
@@ -6,11 +6,13 @@
 <?php foreach($recent_posts as $recent): ?>
 	<div class="bs-post__recent">
 		<?php if(get_post_meta($recent->ID, 'image_square_key', true)): ?>
+		<a href="<?php echo get_permalink($recent->ID) ?>">
 			<img src="<?php echo get_post_meta($recent->ID, 'image_square_key', true) ?>" style="width: 100%" />
+			</a>
 		<?php endif; ?>
 
 		<div class="bs-post__recent__content">
-			<h4><?php echo substr($recent->post_title, 0, 40) ?>...</h4>
+			<h4><a href="<?php echo get_permalink($recent->ID) ?>"><?php echo substr($recent->post_title, 0, 40) ?>...</></h4>
 			<p><?php echo substr(wp_strip_all_tags($recent->post_content), 0, 80) ?>...</p>
 			<a href="<?php echo get_permalink($recent->ID) ?>"><?php echo gett('Read more') ?>...</a>
 		</div>
