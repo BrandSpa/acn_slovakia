ceee86d311877d5d94e17ab49835a22eb74376d7
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-01 16:32:16 -0500

    stop redirect

--- functions.php
+++ functions.php
@@ -150,14 +150,11 @@ function replace_office_texts() {
 function redirectToLang() {
 		$lang = getCountryLang(getCountry());
 	if(!isset($_COOKIE['bs-lang']) && empty($_COOKIE['bs-lang'])) {
-		
 		$url = pll_the_languages( array( 'raw' => 1 ) )[$lang]['url'];
+		setcookie('bs-lang', $lang);
 		header('Location:'. $url);
 		exit;
-	} else {
-		setcookie('bs-lang', $lang);
-	}
-
+	} 
 }
 
 redirectToLang();
