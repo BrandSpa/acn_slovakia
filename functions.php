7847cddca66c44c169ee618caf1fb3012c47ad61
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-01 19:00:16 -0500

    get country

--- functions.php
+++ functions.php
@@ -134,8 +134,8 @@ add_filter('jpeg_quality', function($arg) {
 
 function replace_office_texts() {
 	$text = gett('ACN, during 70 years helping Christians in need');
-	
-	if(bs_in_office()) {
+
+	if(bs_in_office(getCountry())) {
 		$text = gett('TO LEARN MORE ABOUT [office_name], VISIT [office_url]');
 		$text = str_replace('[office_url]', get_option('url_' . space_to_lodash( getCountry() ) ), $text);
 		$text = str_replace('[office_name]', get_option('name_' . space_to_lodash( getCountry() ) ), $text);
