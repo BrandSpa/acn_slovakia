51b0fc655c5336aef7204945e33456811be6ebef
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-02 11:47:17 -0500

    cleanQuote

--- shortcodes/projects.php
+++ shortcodes/projects.php
@@ -18,7 +18,7 @@ function bs_projects_sc($atts, $content = null) {
 
     if(isset($images[$indexImg])) {
       array_push($slides, [
-        'title' => $at['title_' .$i],
+        'title' => cleanQuote($at['title_' .$i]),
         'content' =>  cleanQuote($at['content_' .$i]),
         'imgUrl' => wp_get_attachment_image_src($images[$indexImg], 'full')[0],
       ]);
