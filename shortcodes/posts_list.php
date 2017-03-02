13c84e8303dd373e8621d7e97df7ad36de2ccb63
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-02 16:02:17 -0500

    get wp_count_posts

--- shortcodes/posts_list.php
+++ shortcodes/posts_list.php
@@ -70,6 +70,7 @@ function bs_posts_list_sc($atts, $content = null) {
 
 <?php endforeach; ?>
 	<div class="bs-posts-list__pagination">
+		<?php echo wp_count_posts()->publish; ?>
 		<button class="bs-posts-list__pagination__prev">
 			<i class="ion-chevron-left"></i> <?php echo gett('prev') ?>
 		</button>
