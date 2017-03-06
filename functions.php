e61c8e7f02468541d3e863930eff3fe2e1c76d41
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-06 18:07:07 -0500

    validations

--- functions.php
+++ functions.php
@@ -160,15 +160,16 @@ function cleanQuote($val) {
 }
 
 function geolify($post) {
-	$countries = get_post_meta($post->ID, 'geolify_countries_key', true);
-	$urls = get_post_meta($post->ID, 'geolify_urls_key', true);
-	$key = array_search(getCountry(), $countries);
-	
-	if(isset($urls[$key]) && !empty($urls[$key])) {
-		header('Location:'. $urls[$key]);
-		exit;
+	if($post) {
+		$countries = get_post_meta($post->ID, 'geolify_countries_key', true) ? get_post_meta($post->ID, 'geolify_countries_key', true) : [] ;
+		$urls = get_post_meta($post->ID, 'geolify_urls_key', true);
+		$key = array_search(getCountry(), $countries);
+		
+		if(isset($urls[$key]) && !empty($urls[$key])) {
+			header('Location:'. $urls[$key]);
+			exit;
+		}
 	}
-	
 }
 
 function redirectToLang() {
