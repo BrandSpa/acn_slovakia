11f034f832da95882ad39d01bee65b6fb2643ecc
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-06 17:32:46 -0500

    get countries

--- metaboxes/geolify.php
+++ metaboxes/geolify.php
@@ -17,14 +17,10 @@ function bs_geolify_cb($post) {
   wp_nonce_field('bs_geolify_meta', 'bs_geolify_nonce');
 	$countries = get_post_meta($post->ID, 'geolify_countries_key');
 	$urls = get_post_meta($post->ID, 'geolify_urls_key');
-
-	$props = [
-		"countries" => $countries,
-		"urls" => $urls
-	];
 ?>
+
 <?php var_dump($countries) ?>
-	<div class="bs-geolify" data-props='<?php echo json_encode($props) ?>'></div>
+	<div class="bs-geolify" data-props='{"countries": <?php echo json_encode($countries) ?>}'></div>
 	<script src="<?php echo get_template_directory_uri() ?>/public/js/admin.js"></script>
 <?php
 }
