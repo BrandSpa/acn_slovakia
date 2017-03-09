4969cb21ac2493adab20fc3b4c0b0ee9e1b19c35
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-09 15:35:23 -0500

    add classess

--- client/app.js
+++ client/app.js
@@ -73,11 +73,16 @@ toggleViaCrucisNav();
 let onScroll =  debounce(() => {
   if($('.via-crucis-nav') && $('.nav') && window.outerHeight <= 767) {
     const $nav = $('.nav');
+    const $viaCrucisNav =  $('.via-crucis-nav');
     const $navToggle = $('.via-crucis-toggle');
     let navTop = $nav.offset().top;
     let viaCrucisToggleTop = $navToggle.offset().top; 
     if(navTop > viaCrucisToggleTop) {
       $navToggle.addClass('via-crucis-toggle--fixed');
+      $viaCrucisNav.addClass('via-crucis-nav--fixed');
+    } else {
+      $navToggle.removeClass('via-crucis-toggle--fixed');
+      $viaCrucisNav.removeClass('via-crucis-nav--fixed');
     }
   }
 }, 200);
