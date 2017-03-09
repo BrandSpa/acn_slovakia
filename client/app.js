15f2f81149e2137c32e35434293d93f61370acd7
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-09 15:43:46 -0500

    counts

--- client/app.js
+++ client/app.js
@@ -78,11 +78,13 @@ let onScroll =  debounce(() => {
     let navTop = $nav.offset().top;
     let viaCrucisToggleTop = $navToggle.offset().top; 
     if(navTop > viaCrucisToggleTop) {
+      console.count('pass');
       if(!$navToggle.hasClass('via-crucis-toggle--fixed')) $navToggle.addClass('via-crucis-toggle--fixed');
       if(!$viaCrucisNav.hasClass('via-crucis-nav--fixed')) $viaCrucisNav.addClass('via-crucis-nav--fixed');
     } 
 
     if(navTop < viaCrucisToggleTop) {
+      console.count('dont pass');
       $navToggle.removeClass('via-crucis-toggle--fixed');
       $viaCrucisNav.removeClass('via-crucis-nav--fixed');
     }
