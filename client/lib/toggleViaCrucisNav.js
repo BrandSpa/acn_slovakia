ca4b2bd76b365b18756fd52e4eb6ffe219b15f39
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-10 12:21:38 -0500

    cvhange icon

--- client/lib/toggleViaCrucisNav.js
+++ client/lib/toggleViaCrucisNav.js
@@ -3,6 +3,7 @@ export default function toggleViaCrucisNav() {
   if($('.via-crucis-toggle')) {
      $('.via-crucis-toggle').on('click', e => {
       e.preventDefault();
+       $('.via-crucis-toggle i').toggleClass('ion-chevron-up');
       $('.via-crucis-nav__container').toggleClass( "via-crucis-nav__container--open" );
     })
   }
