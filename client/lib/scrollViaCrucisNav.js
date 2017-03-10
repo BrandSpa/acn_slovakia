313c09f8f9edc0ffb7951f5345c4d3ff3f878a12
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-10 10:50:09 -0500

    change to contaienr

--- client/lib/scrollViaCrucisNav.js
+++ client/lib/scrollViaCrucisNav.js
@@ -4,19 +4,18 @@ export default function scrollViaCrucisNav() {
 	let onScroll = debounce(() => {
   if($('.via-crucis-nav') && $('.nav') && window.outerHeight <= 767) {
     const $nav = $('.nav');
-    const $viaCrucisNav =  $('.via-crucis-nav');
+    const $viaCrucisNav =  $('.via-crucis-nav__container');
     const $navToggle = $('.via-crucis-toggle');
     let navTop = $nav.offset().top;
     let viaCrucisToggleTop = $navToggle.offset().top; 
     let viaCrucisLeft = $('.via-crucis__left').offset().top;
-    console.log('debounce', );
 
     if(navTop > viaCrucisLeft) {
       $navToggle.addClass('via-crucis-toggle--fixed');
-      $viaCrucisNav.addClass('via-crucis-nav--fixed');
+      $viaCrucisNav.addClass('via-crucis-nav__container--fixed');
     } else {
        $navToggle.removeClass('via-crucis-toggle--fixed');
-      $viaCrucisNav.removeClass('via-crucis-nav--fixed');
+      $viaCrucisNav.removeClass('via-crucis-nav__container--fixed');
     }
 
   }
--- client/lib/toggleViaCrucisNav.js
+++ client/lib/toggleViaCrucisNav.js
@@ -3,7 +3,7 @@ export default function toggleViaCrucisNav() {
   if($('.via-crucis-toggle')) {
      $('.via-crucis-toggle').on('click', e => {
       e.preventDefault();
-      $('.via-crucis-nav').toggleClass( "via-crucis-nav--open" );
+      $('.via-crucis-nav__container').toggleClass( "via-crucis-nav__container--open" );
     })
   }
 }
