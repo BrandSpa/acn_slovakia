e18eeeb64958a2a5874923fc36160bd9b0d3ce16
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-14 16:34:41 -0500

    fix infusion

--- apis/index.php
+++ apis/index.php
@@ -3,7 +3,7 @@
 $main_dir = str_replace('apis', '', __DIR__);
 include_once 'mailchimp.php';
 include_once 'stripe.php';
-include_once 'infusionsoft.php';
+include_once 'infusion.php';
 include_once 'posts.php';
 include_once $main_dir . '/lib/countries.php';
 include_once $main_dir . '/lib/get_geoip.php';
@@ -135,6 +135,7 @@ function infusion_contact() {
   $data = $_POST['data'];
   $key = get_option('infusionsoft_key');
   $subdomain = get_option('infusionsoft_subdomain');
+
   $countryTags = [
     'Australia' => '822',
     'Austria' => '824',
