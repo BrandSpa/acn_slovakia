72d657d96e23866baac16479d6a05b15f6f62f32
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-16 10:12:07 -0500

    add pdf on func vc

--- shortcodes/download_pdf.php
+++ shortcodes/download_pdf.php
@@ -1,7 +1,6 @@
 <?php
 
 function bs_download_pdf_sc($atts, $content = null) {
-
   $pdfs = [
     "en" => "http://acninternational.org/wp-content/uploads/2017/03/via_crucis_final_ENGLISH.pdf",
     "es" => "http://acninternational.org/wp-content/uploads/2017/03/via_crucis_final_SPANISH.pdf",
@@ -51,6 +50,13 @@ add_shortcode( 'bs_download_pdf', 'bs_download_pdf_sc' );
 add_action( 'vc_before_init', 'bs_download_pdf_vc' );
 
   function bs_download_pdf_vc() {
+    $pdfs = [
+    "en" => "http://acninternational.org/wp-content/uploads/2017/03/via_crucis_final_ENGLISH.pdf",
+    "es" => "http://acninternational.org/wp-content/uploads/2017/03/via_crucis_final_SPANISH.pdf",
+    "fr" => "http://acninternational.org/wp-content/uploads/2017/03/via_crucis_final_FRENCH.pdf",
+    "de" => "http://acninternational.org/wp-content/uploads/2017/03/via_crucis_final_GERMAN.pdf"
+ ];
+
 		$params = [
       [
         "type" => "textfield",
