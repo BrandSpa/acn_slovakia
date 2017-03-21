f9c6805f93ee1718984d78a35cb48ceb918ec264
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-21 14:29:05 -0500

    cleanquote

--- metaboxes/gallery.php
+++ metaboxes/gallery.php
@@ -21,7 +21,7 @@ function bs_type_gallery_cb($post) {
 
 	<div 
 		class="bs-gallery-metabox" 
-		data-props='<?php echo json_encode(cleanQuote($props)) ?>'>
+		data-props='<?php echo cleanQuote(json_encode($props)) ?>'>
 	</div>
 
 	<script src="<?php echo get_template_directory_uri() ?>/public/js/admin.js"></script>
