1a1ee53b582c27f011afb941fdbe39080371277c
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-21 13:28:19 -0500

    push me

--- templates/post_header_image.php
+++ templates/post_header_image.php
@@ -1,8 +1,8 @@
 <div class="bs-post__header--image" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>)">
 	<div class="bs-post__header--image__title">
 		<h3 style="padding-bottom: 20px; border-bottom: 1px solid #D3D3D3"><?php the_title() ?></h3>
-				<a style="display: block; margin: 0 auto; width: 20px" href="#post-content">
-	
+		<a style="display: block; margin: 0 auto; width: 20px" href="#post-content">
+			<?php require('down_arrow.php') ?>
 		</a>
 	</div>
 </div>
