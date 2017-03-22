2bbc6d0b67478c1ebd37c15005f3ccbfc2e4d530
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-22 14:06:46 -0500

    block

--- public/js/app.js
+++ public/js/app.js
@@ -28703,7 +28703,7 @@ var GalleryHeader = _react2.default.createClass({
 								}),
 								_react2.default.createElement(
 									'span',
-									{ className: 'header-gallery__caption-text', style: { color: '#fff', marginTop: '40px' } },
+									{ className: 'header-gallery__caption-text', style: { color: '#fff', marginTop: '20px', display: 'block' } },
 									excerpts[i]
 								),
 								_react2.default.createElement('a', {
