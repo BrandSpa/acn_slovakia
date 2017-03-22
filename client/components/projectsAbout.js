dcaa0fd7d47b830be43a9b9cf08afe80a4fb274d
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-22 17:56:34 -0500

    add nums

--- client/components/projectsAbout.js
+++ client/components/projectsAbout.js
@@ -4,13 +4,15 @@ import Projects from './projects';
 const ProjectsAbout = React.createClass({
 	render() {
 		return (
-			<Projects contents={this.props.projects} donate={this.props.donate} />
-			<div class="projects-about-num">
-				<div class="projects-about-num__num">
-					{this.props.projects[1].number}
-				</div>
-				<div class="projects-about-num__text">
-					{this.props.projects[1].number_text}
+			<div>
+				<Projects contents={this.props.projects} donate={this.props.donate} />
+				<div class="projects-about-num">
+					<div class="projects-about-num__num">
+						{this.props.projects[1].number}
+					</div>
+					<div class="projects-about-num__text">
+						{this.props.projects[1].number_text}
+					</div>
 				</div>
 			</div>
 		)
