336a01744bbe6de5292a50f98e73da7a04ff7911
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-22 15:40:55 -0500

    only video and gallery

--- client/components/posts.js
+++ client/components/posts.js
@@ -11,7 +11,7 @@ const Posts = React.createClass({
   },
 
   componentWillMount() {
-    let data = qs.stringify({action: 'get_posts', post_type: ['video', 'gallery', 'post']});
+    let data = qs.stringify({action: 'get_posts', post_type: ['video', 'gallery']});
 
     request
       .post('/wp-admin/admin-ajax.php', data)
