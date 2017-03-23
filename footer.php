8096efc2873b539a0b1348877d6024fa5ffe1a78
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-23 14:32:21 -0500

    last app version

--- footer.php
+++ footer.php
@@ -42,7 +42,7 @@
   bs.donate = '<?php echo gett('Donate') ?>'
 </script>
 
-<script src='<?php echo get_template_directory_uri() ?>/public/js/app.20b2e8c9931c88908721.js'></script>
+<script src='<?php echo get_template_directory_uri() ?>/public/js/app.cf92d634ea56a884b87b.js'></script>
 
 <!--/app theme-->
 
