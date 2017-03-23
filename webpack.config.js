f2b13fd541e66cbf0d24f7585077a8fb0d8436cb
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-23 14:59:17 -0500

    get app

--- webpack.config.js
+++ webpack.config.js
@@ -28,13 +28,16 @@ module.exports = {
           
           if (!stats.errors.length) {
             var htmlFileName = "footer.php";
-            var html = fs.readFileSync(Path.join(__dirname, htmlFileName), "utf8");
-            var lastHash = fs.readFileSync(Path.join(__dirname, 'lasthash.txt'), "utf8");
             var lastFile = stats.chunks[0].files[0];
+            fs.writeFileSync( Path.join(__dirname, 'lasthash.txt'), lastFile);
+
             if(fs.existsSync(Path.join(__dirname, 'public/js/' + lastHash ))) {
               fs.unlinkSync(Path.join(__dirname, 'public/js/' + lastHash ));
             } 
               
+            var html = fs.readFileSync(Path.join(__dirname, htmlFileName), "utf8");
+            var lastHash = fs.readFileSync(Path.join(__dirname, 'lasthash.txt'), "utf8");
+            var lastFile = stats.chunks[0].files[0];
             fs.writeFileSync( Path.join(__dirname, 'lasthash.txt'), lastFile);
 
             var htmlOutput = html.replace(
