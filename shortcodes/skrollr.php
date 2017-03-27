ce96467c7c7cb7211a4e5c8243e22b16519ce4e6
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-27 17:51:52 -0500

    my last changes

--- shortcodes/skrollr.php
+++ shortcodes/skrollr.php
@@ -19,8 +19,8 @@ function bs_skrollr_sc($atts, $content = null) {
 
 
 <div class="skrollr_container">
-    <?php foreach($at as $key=>$titulo){//$key = ($key+1) * 200; 
-    var_dump($key); ?>
+    <?php 
+    foreach($at as $key => $titulo){$key = ($key+1) * 200;  ?>
         <h1 data-0="transform:rotate(0deg);" data-<?php echo $key;?> ="transform:rotate(360deg);"><?php echo($titulo);?></h1><br />
     <?php } ?>
 
