43b37f16012380e16d62415051ddbd65ca5165f2
Julian Poveda <hjulianpoveda@gmail.com>
2017-03-27 17:54:07 -0500

    initial scroll position

--- shortcodes/skrollr.php
+++ shortcodes/skrollr.php
@@ -23,7 +23,7 @@ function bs_skrollr_sc($atts, $content = null) {
     $increme=0;
     foreach($at as $titulo){ 
          $increme+=200; ?>
-        <h1 data-0="transform:rotate(0deg);" data-<?php echo $increme;?> ="transform:rotate(360deg);"><?php echo($titulo);?></h1><br />
+        <h1 data-<?php echo $increme-200;?>="transform:rotate(0deg);" data-<?php echo $increme;?> ="transform:rotate(360deg);"><?php echo($titulo);?></h1><br />
 
     <?php } ?>
 
