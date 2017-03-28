13610f2730369e66da8c8179572aead9376ba52f
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-28 10:32:58 -0500

    put in work

--- client/components/galleryHeader.js
+++ client/components/galleryHeader.js
@@ -105,7 +105,7 @@ const GalleryHeader = React.createClass({
 								style={i == this.state.section ? {display: 'block', position: 'relative'} : {display: 'none'}}
 							>
 
-								<img src={image} alt="" style={{width: 'auto', maxWidth: '100%', height: '100%'}} />
+								<img src={image} alt="" style={{maxWidth: '100%', height: 'auto'}} />
 								<span style={excerptStyle}>{excerpts[i]}</span>
 
 								<a href="#" onClick={this.changeSection.bind(null, 'prev')} style={linkLeft}></a> 
--- footer.php
+++ footer.php
@@ -42,8 +42,8 @@
   bs.donate = '<?php echo gett('Donate') ?>'
 </script>
 
-<script src='<?php echo get_template_directory_uri() ?>/public/js/vendor.c2ee0b06fc9a7ad0c7df.js'></script>
-<script src='<?php echo get_template_directory_uri() ?>/public/js/app.60b7539b2a45a968e52e.js'></script>
+<script src='<?php echo get_template_directory_uri() ?>/public/js/vendor.4dcf1c60974d2dca9e39.js'></script>
+<script src='<?php echo get_template_directory_uri() ?>/public/js/app.7557917f1dc04d3b9193.js'></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flexslider/2.6.3/jquery.flexslider.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/skrollr/0.6.30/skrollr.min.js"></script>
 <script>
