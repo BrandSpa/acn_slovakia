5cea40a58bd3b1c1e9dc4907a0fe49200bb46f57
Julian Poveda <hjulianpoveda@gmail.com>
2017-03-29 17:20:18 -0500

    onLoad Method added to jquery

--- single-featured.php
+++ single-featured.php
@@ -37,11 +37,11 @@
 </div>
 
 <script>
-    jQuery('document').load(function() {
-        jQuery('body').css('padding', 0);
-        jQuery('.nav').css('background-color', 'rgba(60,81,95,.7)');
-        jQuery('.nav a').css('color', '#FFF');
-        jQuery('.nav img').css('filter', 'grayscale() invert()');
+    onLoad(function() {
+        $('body').css('padding', 0);
+        $('.nav').css('background-color', 'rgba(60,81,95,.7)');
+        $('.nav a').css('color', '#FFF');
+        $('.nav img').css('filter', 'grayscale() invert()');
     });
 
 </script>
