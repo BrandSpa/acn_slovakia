737ea5b5b96992537e8303e4f763114bde230e40
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-30 15:27:33 -0500

    add metada to gallery type

--- single-gallery.php
+++ single-gallery.php
@@ -59,12 +59,20 @@ onLoad(function() {
 
 <div class="l-wrap">
 
-	<div class="breadcrumbs" style="margin-top: 20px; text-align: center; color: #b9b9b9" typeof="BreadcrumbList" vocab="https://schema.org/">
-    <?php if(function_exists('bcn_display'))
-    {
-        bcn_display();
-    }?>
-</div>
+<span class="video__header__metadata" style="font-size: 1.1em; color: #4A4A4A">
+				<?php foreach(get_the_category($post->ID) as $ind => $category): ?>
+					<span style="font-weight: 600">
+						<?php echo $category->name ?> <?php echo $ind >= 0 && $ind + 1 != count(get_the_category($post->ID)) ?  '|' : '' ?>
+					</span>
+				<?php endforeach; ?>
+				<?php echo '/ ' . get_the_date( 'm - Y', $post->ID ); ?>
+			</span>
+			<div class="breadcrumbs" style="margin: 3px 0 10px 0;  color: #b9b9b9" typeof="BreadcrumbList" vocab="https://schema.org/">
+			<?php if(function_exists('bcn_display')) { bcn_display(); }?>
+		</div>
+
+		<h3 style="padding-bottom: 10px;color: #3C515F"><?php the_title() ?></h3>
+
 
 		<div class="bs-post__content col-8-l col-12-s" id="post-content">
 			<?php the_content() ?>
