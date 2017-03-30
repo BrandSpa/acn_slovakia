82a2ffeeb5f5131d2cfcf5fd5b7843ee0919802c
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-30 16:06:09 -0500

    add styles

--- templates/post_share.php
+++ templates/post_share.php
@@ -3,19 +3,51 @@
 	<div class="l-wrap">
 		<ul>
 			<li style="display: inline-block">
-      	<a key={1} style={shareBtn} href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $current_url ?>">
+      	<a key={1} style="	color: #fff;
+			width: 30px;
+			height: 30px;
+			borderRadius: 30px;
+			textAlign: center;
+			display: block;
+			border: 1px solid #fff;
+			padding: 5px;
+      fontSize: 12px;" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $current_url ?>">
         	<i class="ion-social-facebook"></i></a>
       </li>
 			<li style="display: inline-block">
-				<a key={2} style={shareBtn} href="https://twitter.com/intent/tweet?text=<?php echo $current_url ?>">
+				<a key={2} style="	color: #fff;
+			width: 30px;
+			height: 30px;
+			borderRadius: 30px;
+			textAlign: center;
+			display: block;
+			border: 1px solid #fff;
+			padding: 5px;
+      fontSize: 12px;" href="https://twitter.com/intent/tweet?text=<?php echo $current_url ?>">
 					<i class="ion-social-twitter"></i></a>
 			</li>
 			<li style="display: inline-block">
-				<a key={3} style={shareBtn} href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $current_url ?>">
+				<a key={3} style="	color: #fff;
+			width: 30px;
+			height: 30px;
+			borderRadius: 30px;
+			textAlign: center;
+			display: block;
+			border: 1px solid #fff;
+			padding: 5px;
+      fontSize: 12px;" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $current_url ?>">
 					<i class="ion-social-linkedin"></i></a>
 			</li>
-			<li style={{...liStyle, display: 'none', '@media(max-width: 767px)': {display: 'inline-block'} }}>
-				<a key={4} style={shareBtn} href="whatsapp://send?text=<?php echo $current_url ?>">
+			<li style="display: inline-block">
+				<a key={4} style="	color: #fff;
+			width: 30px;
+			height: 30px;
+			borderRadius: 30px;
+			textAlign: center;
+			display: block;
+			border: 1px solid #fff;
+			padding: 5px;
+      fontSize: 12px;" href="whatsapp://send?text=<?php echo $current_url ?>">
 					<i class="ion-social-whatsapp-outline"></i>
 				</a>
 			</li>
