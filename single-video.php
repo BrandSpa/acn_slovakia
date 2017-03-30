b5fd1b4008e44647169d282cac544729ba4b0be0
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-30 11:22:23 -0500

    margin top instead of top

--- single-video.php
+++ single-video.php
@@ -11,16 +11,19 @@
 }'></div>
 
 	<div class="l-wrap">
-	<div class="video__header-title">
-		<span class="video__header__metadata" style="font-size: 1.1em; color: #4A4A4A">
-		<?php foreach(get_the_category($post->ID) as $ind => $category): ?>
-		<span style="font-weight: 600"><?php echo $category->name ?> <?php echo $ind >= 0 && $ind + 1 != count(get_the_category($post->ID)) ?  '|' : '' ?></span>
-		<?php endforeach; ?>
-		/ <?php echo get_the_date( 'm - Y', $post->ID ); ?>
-		</span>
-		<div class="breadcrumbs" style="margin: 3px 0 10px 0;  color: #b9b9b9" typeof="BreadcrumbList" vocab="https://schema.org/">
-    <?php if(function_exists('bcn_display')) { bcn_display(); }?>
-</div>
+		<div class="video__header-title">
+			<span class="video__header__metadata" style="font-size: 1.1em; color: #4A4A4A">
+				<?php foreach(get_the_category($post->ID) as $ind => $category): ?>
+					<span style="font-weight: 600">
+						<?php echo $category->name ?> <?php echo $ind >= 0 && $ind + 1 != count(get_the_category($post->ID)) ?  '|' : '' ?>
+					</span>
+				<?php endforeach; ?>
+				<?php echo '/ ' . get_the_date( 'm - Y', $post->ID ); ?>
+			</span>
+			<div class="breadcrumbs" style="margin: 3px 0 10px 0;  color: #b9b9b9" typeof="BreadcrumbList" vocab="https://schema.org/">
+			<?php if(function_exists('bcn_display')) { bcn_display(); }?>
+		</div>
+
 		<h3 style="padding-bottom: 10px;color: #3C515F"><?php the_title() ?></h3>
 		<a style="display: block; margin: 0 auto; width: 20px" href="#post-content">
 			<?php require(__DIR__. '/templates/down_arrow.php')?>
@@ -36,7 +39,7 @@
 		var titleH = $('.video__header-title').innerHeight();
 	
 		if(window.innerWidth > '700') {
-			$('.video__header-title').css({position: 'relative', top: '-' + titleH + 'px'});
+			$('.video__header-title').css({position: 'relative', marginTop: '-' + titleH + 'px'});
 		}
 	})
 	</script>
