9d2fb647757375893e7eb0e3badec6f9826a55af
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-30 10:35:57 -0500

    change margins and padding

--- single-video.php
+++ single-video.php
@@ -11,14 +11,14 @@
 }'></div>
 
 	<div class="l-wrap">
-	<div class="video__header-title" style="background: #fff; min-height: 100px; float: left; width: 100%;padding: 20px 80px 0 80px">
+	<div class="video__header-title" style="background: #fff; min-height: 100px; float: left; width: 100%;padding: 40px 80px 0 80px">
 		<span class="video__header__metadata" style="font-size: 18px; color: #4A4A4A">
 		<?php foreach(get_the_category($post->ID) as $ind => $category): ?>
 		<span style="font-weight: 600"><?php echo $category->name ?> <?php echo $ind >= 0 && $ind + 1 != count(get_the_category($post->ID)) ?  '|' : '' ?></span>
 		<?php endforeach; ?>
 		/ <?php echo get_the_date( 'm - Y', $post->ID ); ?>
 		</span>
-		<div class="breadcrumbs" style="margin: 10px 0 20px 0;  color: #b9b9b9" typeof="BreadcrumbList" vocab="https://schema.org/">
+		<div class="breadcrumbs" style="margin: 5px 0 20px 0;  color: #b9b9b9" typeof="BreadcrumbList" vocab="https://schema.org/">
     <?php if(function_exists('bcn_display')) { bcn_display(); }?>
 </div>
 		<h3 style="padding-bottom: 10px;color: #3C515F"><?php the_title() ?></h3>
