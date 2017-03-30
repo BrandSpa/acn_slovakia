4ac8319ac51bd0c9d9b28ff8547b5a68dc5bbf00
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-30 16:07:36 -0500

    change color

--- templates/post_share.php
+++ templates/post_share.php
@@ -3,49 +3,49 @@
 	<div class="l-wrap">
 		<ul>
 			<li style="display: inline-block">
-      	<a key={1} style="	color: #fff;
+      	<a key={1} style="	color: #939597;
 			width: 30px;
 			height: 30px;
 			borderRadius: 30px;
 			textAlign: center;
 			display: block;
-			border: 1px solid #fff;
+			border: 1px solid #939597;
 			padding: 5px;
       fontSize: 12px;" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $current_url ?>">
         	<i class="ion-social-facebook"></i></a>
       </li>
 			<li style="display: inline-block">
-				<a key={2} style="	color: #fff;
+				<a key={2} style="	color: #939597;
 			width: 30px;
 			height: 30px;
 			borderRadius: 30px;
 			textAlign: center;
 			display: block;
-			border: 1px solid #fff;
+			border: 1px solid #939597;
 			padding: 5px;
       fontSize: 12px;" href="https://twitter.com/intent/tweet?text=<?php echo $current_url ?>">
 					<i class="ion-social-twitter"></i></a>
 			</li>
 			<li style="display: inline-block">
-				<a key={3} style="	color: #fff;
+				<a key={3} style="	color: #939597;
 			width: 30px;
 			height: 30px;
 			borderRadius: 30px;
 			textAlign: center;
 			display: block;
-			border: 1px solid #fff;
+			border: 1px solid #939597;
 			padding: 5px;
       fontSize: 12px;" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $current_url ?>">
 					<i class="ion-social-linkedin"></i></a>
 			</li>
 			<li style="display: inline-block">
-				<a key={4} style="	color: #fff;
+				<a key={4} style="	color: #939597;
 			width: 30px;
 			height: 30px;
 			borderRadius: 30px;
 			textAlign: center;
 			display: block;
-			border: 1px solid #fff;
+			border: 1px solid #939597;
 			padding: 5px;
       fontSize: 12px;" href="whatsapp://send?text=<?php echo $current_url ?>">
 					<i class="ion-social-whatsapp-outline"></i>
