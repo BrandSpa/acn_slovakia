3b8ea03a533abda71b68ece4e7c793313a1a232e
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-31 16:56:50 -0500

    add create event

--- apis/cl.php
+++ apis/cl.php
@@ -8,21 +8,7 @@ $dir_base =  str_replace('apis', '', __DIR__);
 
 require $dir_base . 'vendor/autoload.php';
 
-	function cl_createPerson($appId, $apiKey, $data) {
-		try {
-		$cl = new \ConvertLoop\ConvertLoop($appId, $apiKey, "v1");	
-		$person = array(
-			"email" => $data['email'],
-			"first_name" => $data['name'],
-			"last_name" => $data['lastname'],
-		);
-
-		$cl->people()->createOrUpdate($person);	
-		} catch(Exception $e) {
-			return $e;
-		}
-	}
-
+	// $data: { "email": "german.escobar@convertloop.co", "add_tags": ["Tag 1", "Tag2"] }
 	function cl_create_person($appId, $apiKey, $data) {
 		try {
 			$data = json_encode($data);
@@ -31,7 +17,8 @@ require $dir_base . 'vendor/autoload.php';
 
 			$headers = array(
 				"Authorization" => "Basic " . $auth,
-				'Accept' => 'application/json', 'content-type' => 'application/json'
+				'Accept' => 'application/json', 
+				'content-type' => 'application/json'
 			);
 
 			$endpoint = 'https://api.convertloop.co/v1/people';
@@ -44,12 +31,26 @@ require $dir_base . 'vendor/autoload.php';
 		
 	}
 
-	function cl_createEvent($appId, $apiKey, $data) {
-		$cl = new \ConvertLoop\ConvertLoop($appId, $apiKey, "v1");
-		$cl->eventLogs()->send(array(
-			"name" => $data['event'], 
-			"email" => $data['email']
-		));
+	// $data: { "name": "Signed Up", "person": { "email": "german.escobar@convertloop.co" } }
+	function cl_create_event($appId, $apiKey, $data) {
+			try {
+			$data = json_encode($data);
+			$auth_string = $appId . ":" . $apiKey;
+      $auth = base64_encode($auth_string);
+
+			$headers = array(
+				"Authorization" => "Basic " . $auth,
+				'Accept' => 'application/json', 
+				'content-type' => 'application/json'
+			);
+
+			$endpoint = 'https://api.convertloop.co/v1/event_logs';
+
+			$req = Requests::post($endpoint . '', $headers, $data);
+			return $req->body;
+		} catch(Exception $e) {
+			return ['error' => $e];
+		}
 	}
 
 
