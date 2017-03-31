9bd33e8035dd2eb257d2e96aaadae4cb3027a0ab
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-31 11:45:08 -0500

    add api to convertloop

--- apis/convertloop.php
+++ apis/convertloop.php
@@ -2,11 +2,29 @@
 $dir_base =  str_replace('apis', '', __DIR__);
 require $dir_base . 'vendor/autoload.php';
 
-function createPerson() {
-	$cl = new \ConvertLoop\ConvertLoop("746fffe4", "NFZRSq3xfbmHudRAUYtLkBCv", "v1");	
+function createPerson($appId, $apiKey, $data) {
+	$cl = new \ConvertLoop\ConvertLoop($appId, $apiKey, "v1");	
+	$person = array(
+    "email" => $data['email'],
+    "first_name" => $data['name'],
+    "last_name" => $data['lastname'],
+	);
+
+	$cl->people()->createOrUpdate($person);	
+}
+
+function createPersonWithTags($appId, $apiKey, $data) {
+	$cl = new \ConvertLoop\ConvertLoop($appId, $apiKey, "v1");	
 	$cl->people()->createOrUpdate(array(
-  	"email" => "german.escobar@convertloop.co",
-  	"add_tags" => array("Tag 1", "Tag 2")
+  	"email" => $data['email'],
+  	"add_tags" => $data['tags'] //is an array
 	));
 }
 
+function createEvent($appId, $apiKey, $data) {
+	$cl = new \ConvertLoop\ConvertLoop($appId, $apiKey, "v1");
+	$cl->eventLogs()->send(array(
+		"name" => $data['event'], 
+		"email" => $data['email']
+	));
+}
