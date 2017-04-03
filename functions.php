b6424de3c3f7c590afcbf95dbe2861480a6a3cde
Alejandro Sanabria <alesanabriav@gmail.com>
2017-04-03 09:45:12 -0500

    wp_doing_ajax polyfill?

--- functions.php
+++ functions.php
@@ -232,6 +232,20 @@ function redirectToLang() {
 **	Not redirect if the call is via ajax
 **/
 
+if(!function_exists('wp_doing_ajax')) {
+	function wp_doing_ajax() {
+  
+	/**
+	* Filters whether the current request is a WordPress Ajax request.
+	*
+	* @since 4.7.0
+	*
+	* @param bool $wp_doing_ajax Whether the current request is a WordPress Ajax request.
+	*/
+		return apply_filters( 'wp_doing_ajax', defined( 'DOING_AJAX' ) && DOING_AJAX );
+	}
+}
+
 if(function_exists('wp_doing_ajax') && !wp_doing_ajax()) {
 	redirectToLang();
 }
