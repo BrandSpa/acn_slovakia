ecfbbca535d3e69d2e0256bbeb91b39fc84933c1
Alejandro Sanabria <alesanabriav@gmail.com>
2017-04-03 18:37:31 -0500

    add more margin

--- single-gallery.php
+++ single-gallery.php
@@ -76,7 +76,7 @@ onLoad(function() {
 	</div>
 </div>
 
-	<div class="l-wrap" style="margin-top: 40px">
+	<div class="l-wrap" style="margin-top: 80px">
 		<div class="col-1-l"></div>
 		<div class="col-5-l col-12-s" style="word-wrap: break-word;">
 			<?php the_content() ?>
