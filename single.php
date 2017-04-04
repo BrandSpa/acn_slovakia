6b237e110efd9c107c3417a50af029ae70f65918
Julian Poveda <hjulianpoveda@gmail.com>
2017-04-04 18:52:26 -0500

    added video header styles

--- single.php
+++ single.php
@@ -18,30 +18,25 @@
     }?>
 </div>-->
 <!-- Nuevo Jul -->
-<div class="bs-post__header--image" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>); background-attachment: fixed">
-    <div class="bs-feat__img__textbox" style="">
-        <div class="news_date" style="color:#FFF;">
-        <span class="featured__metadata" style="font-weight:600; font-size:18px; color:#eaeaea line-height:1.28; text-align:left;">
+<div class="l-wrap">
+		<div class="video__header-title">
+			<span class="video__header__metadata" style="font-size: 1.1em; color: #4A4A4A">
 				<?php foreach(get_the_category($post->ID) as $ind => $category): ?>
-					<span style="">
+					<span style="font-weight: 600">
 						<?php echo $category->name ?> <?php echo $ind >= 0 && $ind + 1 != count(get_the_category($post->ID)) ?  '|' : '' ?>
 					</span>
 				<?php endforeach; ?>
-				<?php echo '/ ' . get_the_date( 'M Y', $post->ID ); ?>
+				<?php echo '/ ' . get_the_date( 'm - Y', $post->ID ); ?>
 			</span>
-        </div>
-        <div class="breadcrumbs" style=" color: #e1e1e1; text-align:left; font-size:15px; margin:0;" typeof="BreadcrumbList" vocab="https://schema.org/">
-        <?php if(function_exists('bcn_display'))
-        {
-         bcn_display();
-        }?>
-        </div>
-        <h3 style="color:#FFF; font-size:40px; line-height:1.05; margin:40px 0;"><?php the_title(); ?></h3>
-    </div> 
-    <a class="bs-down-btn" href="#post-content">
+			<div class="breadcrumbs" style="margin: 3px 0 10px 0;  color: #b9b9b9" typeof="BreadcrumbList" vocab="https://schema.org/">
+			<?php if(function_exists('bcn_display')) { bcn_display(); }?>
+		</div>
+
+		<h3 style="padding-bottom: 10px;color: #3C515F"><?php the_title() ?></h3>
+		<a style="display: block; margin: 0 auto; width: 20px" href="#post-content">
 			<?php require(__DIR__. '/templates/down_arrow.php')?>
-	</a>
-</div>
+		</a>
+	</div>
 <!-- Fin Nuevo-->
 
 		<div class="bs-post__content col-8-l col-12-s" id="post-content">
