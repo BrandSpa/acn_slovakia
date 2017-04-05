c33156344c657536951b21cad4a76fd9219fce96
Julian Poveda <hjulianpoveda@gmail.com>
2017-04-04 19:19:44 -0500

    File include tests

--- templates/post_header.php
+++ templates/post_header.php
@@ -1,27 +1,8 @@
-<!--<div class="l-wrap">
+<div class="l-wrap">
 	<div class="bs-post__header col-12-l">
 		<div class="bs-post__header__title col-10-l col-12-s">
-			<h2><?php //the_title() ?></h2>
+			<h2><?php the_title() ?></h2>
 		</div>
 	</div>
-</div>-->
+</div>
 
-<div class="l-wrap">
-		<div class="video__header-title">
-			<span class="video__header__metadata" style="font-size: 1.1em; color: #4A4A4A">
-				<?php foreach(get_the_category($post->ID) as $ind => $category): ?>
-					<span style="font-weight: 600">
-						<?php echo $category->name ?> <?php echo $ind >= 0 && $ind + 1 != count(get_the_category($post->ID)) ?  '|' : '' ?>
-					</span>
-				<?php endforeach; ?>
-				<?php echo '/ ' . get_the_date( 'm - Y', $post->ID ); ?>
-			</span>
-			<div class="breadcrumbs" style="margin: 3px 0 10px 0;  color: #b9b9b9" typeof="BreadcrumbList" vocab="https://schema.org/">
-			<?php if(function_exists('bcn_display')) { bcn_display();}?>
-		</div>
-
-		<h3 style="padding-bottom: 10px;color: #3C515F"><?php the_title() ?></h3>
-		<a style="display: block; margin: 0 auto; width: 20px" href="#post-content">
-			<?php require(__DIR__. '/templates/down_arrow.php')?>
-		</a>
-	</div>
--- templates/post_header_image.php
+++ templates/post_header_image.php
@@ -1,9 +1,24 @@
 <div class="bs-post__header--image" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>)">
 	<div class="bs-post__header--image__title">
-		<h3 style="padding-bottom: 20px; border-bottom: 1px solid #D3D3D3"><?php the_title() ?></h3>
+		<div class="l-wrap">
+		<div class="video__header-title">
+			<span class="video__header__metadata" style="font-size: 1.1em; color: #4A4A4A">
+				<?php foreach(get_the_category($post->ID) as $ind => $category): ?>
+					<span style="font-weight: 600">
+						<?php echo $category->name ?> <?php echo $ind >= 0 && $ind + 1 != count(get_the_category($post->ID)) ?  '|' : '' ?>
+					</span>
+				<?php endforeach; ?>
+				<?php echo '/ ' . get_the_date( 'm - Y', $post->ID ); ?>
+			</span>
+			<div class="breadcrumbs" style="margin: 3px 0 10px 0;  color: #b9b9b9" typeof="BreadcrumbList" vocab="https://schema.org/">
+			<?php if(function_exists('bcn_display')) { bcn_display();}?>
+		</div>
+
+		<h3 style="padding-bottom: 10px;color: #3C515F"><?php the_title() ?></h3>
 		<a style="display: block; margin: 0 auto; width: 20px" href="#post-content">
-			<?php require('down_arrow.php') ?>
+			<?php require(__DIR__. '/templates/down_arrow.php')?>
 		</a>
+	</div>
 	</div>
 </div>
 
