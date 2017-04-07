df7fb5a84e7acddbef8f64367a10c99468fd65c6
Julian Poveda <hjulianpoveda@gmail.com>
2017-04-07 15:18:23 -0500

    ready methos used instead

--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -75,7 +75,7 @@
   Lightbox.prototype.init = function() {
     var self = this;
     // Both enable and build methods require the body tag to be in the DOM.
-    jQuery(document).onLoad(function() {
+    jQuery(document).ready(function() {
       self.enable();
       self.build();
     });
