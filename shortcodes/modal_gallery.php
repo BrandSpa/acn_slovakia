06228fd9f9269a1493b591a30f97c382f45755ae
Julian Poveda <hjulianpoveda@gmail.com>
2017-04-07 17:07:20 -0500

    semicolons updated

--- shortcodes/modal_gallery.php
+++ shortcodes/modal_gallery.php
@@ -37,7 +37,7 @@ function bs_modal_gallery_sc($atts, $content = null) {
         <img src="<?php echo wp_get_attachment_url($image) ?>" alt="<?php $attachment_meta['alt']; ?>" />
       </a>
 
-      <a href="<?php echo wp_get_attachment_url($image) ?> rel="lightbox" ?>">image #1</a>
+      <a href="<?php echo wp_get_attachment_url($image) ?> rel='lightbox'>">image #1</a>
 		<?php
 		endforeach;
 		?>
