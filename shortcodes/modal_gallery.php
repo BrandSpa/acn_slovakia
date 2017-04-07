d9cdd54201f65f07d089d24e60acdbcb9d26bfe3
Julian Poveda <hjulianpoveda@gmail.com>
2017-04-07 01:10:20 -0500

    variable not passing

--- shortcodes/modal_gallery.php
+++ shortcodes/modal_gallery.php
@@ -14,9 +14,11 @@ function bs_modal_gallery_sc($atts, $content = null) {
 
 <!-- Place somewhere in the <body> of your page -->
 <div class="flexslider">
+<h3>Wdjdhsakj</h3>
+<h2> <?php echo $at['groupName'] ?> </h2>
   <ul class="slides">
-        <?php $lGroupName = $at['groupName']?>
-        <?php echo $lGroupName  ?>
+
+        
 		<?php foreach(explode(',', $at['images']) as $image):?>
 		 <li>
      	 <img src="<?php echo wp_get_attachment_url($image)  ?>" data-lightbox="<?php echo $lGroupName[0]  ?>" rel="prueba" alt="<?php echo get_post_meta($image, '_wp_attachment_image_alt', true);;  ?>" data-title="" />
