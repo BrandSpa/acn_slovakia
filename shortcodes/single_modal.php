9e727f4362c4c5119de6a55ed6ade6673cb87419
Julian Poveda <hjulianpoveda@gmail.com>
2017-04-10 16:53:53 -0500

    single modification not building yarn

--- shortcodes/single_modal.php
+++ shortcodes/single_modal.php
@@ -12,18 +12,7 @@ function bs_single_modal_sc($atts, $content = null) {
 ?>
 
 <?php 
-  function wp_get_attachment( $attachment_id ) {
-
-    $attachment = get_post( $attachment_id );
-    return array(
-    'alt' => get_post_meta( $attachment->ID, '_wp_attachment_image_alt', true ),
-    'caption' => $attachment->post_excerpt,
-    'description' => $attachment->post_content,
-    'href' => get_permalink( $attachment->ID ),
-    'src' => $attachment->guid,
-    'title' => $attachment->post_title
-);
-}
+ 
 ?>
 
 <!-- Place somewhere in the <body> of your page -->
