079679ecf66ddb56e46fe89e3c1bd04844aab159
Julian Poveda <hjulianpoveda@gmail.com>
2017-04-10 17:05:28 -0500

    single image load

--- shortcodes/single_modal.php
+++ shortcodes/single_modal.php
@@ -3,7 +3,7 @@ add_shortcode( 'bs_single_modal', 'bs_single_modal_sc' );
 
 function bs_single_modal_sc($atts, $content = null) {
 	$attributes = [
-    'images' => ''
+    'image' => ''
   ];
 
   $at = shortcode_atts( $attributes , $atts );
@@ -18,27 +18,15 @@ function bs_single_modal_sc($atts, $content = null) {
 <!-- Place somewhere in the <body> of your page -->
 <!--<link href="<?php //echo get_template_directory_uri() ?>/public/css/lightbox.css" rel="stylesheet">-->
 <div class="single_modal" style="margin:20px 0;">
-    <?php $countmodal=0;?>
-		<?php foreach(explode(',', $at['images']) as $image): ?>
+    <?php //$countmodal=0;?>
+		<?php $image = $at['image'];//foreach(explode(',', $at['images']) as $image): ?>
         <?php $attachment_meta = wp_get_attachment($image); ?>
 		 
-      <!--<a href="<?php echo wp_get_attachment_url($image) ?>" data-lightbox="<?php// echo $at['groupname'] ?>" data-title="<?php //$attachment_meta['description']; ?>">
-        <img src="<?php echo wp_get_attachment_url($image) ?>" alt="<?php //$attachment_meta['alt']; ?>" />
-      </a>-->
 
-      <a href="<?php echo wp_get_attachment_url($image) ?>" rel="lightbox[<?php echo $at['groupname'] ?>]" title="<?php echo $attachment_meta['description']; ?>">
-      <?php if($countmodal==0)
-      {?>
+      <a href="<?php echo wp_get_attachment_url($image) ?>" rel="lightbox['sameGroup']" title="<?php echo $attachment_meta['description']; ?>">
         <img style="max-width:100%;" src="<?php echo wp_get_attachment_url($image) ?>" alt="<?php echo $attachment_meta['alt']; ?>" />
-        <div class="modal_text" style="position:absolute; top:15px; padding:20px; color:#FFF;"><h5>+ Photo Gallery</h5></div>
-      <?php }else{?>
-        <img style="display:none;" src="<?php echo wp_get_attachment_url($image) ?>" alt="<?php echo $attachment_meta['alt']; ?>" />
-      <?php }?>
+        <!--<div class="modal_text" style="position:absolute; top:15px; padding:20px; color:#FFF;"><h5>+ Photo Gallery</h5></div>-->
       </a>
-		<?php
-    $countmodal++;
-		endforeach;
-		?>
 </div>
 
 <script>
@@ -59,9 +47,9 @@ add_action( 'vc_before_init', 'bs_single_modal_vc' );
   function bs_single_modal_vc() {
 		$params = [
 			[
-        "type" => "attach_images",
-        "heading" => "Images",
-        "param_name" => "images",
+        "type" => "attach_image",
+        "heading" => "Image",
+        "param_name" => "image",
         "value" => ''
 			],
  
