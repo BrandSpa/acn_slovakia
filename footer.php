8de275ce589ba7656cbc6433da3504b2de95d9b0
Julian Poveda <hjulianpoveda@gmail.com>
2017-04-19 15:17:05 -0500

    yarn build

--- footer.php
+++ footer.php
@@ -50,8 +50,8 @@
   bs.donate = '<?php echo gett('Donate') ?>'
 </script>
 
-<script src='<?php echo get_template_directory_uri() ?>/public/js/vendor.9e7f13dd226978f7408c.js'></script>
-<script src='<?php echo get_template_directory_uri() ?>/public/js/app.f53c924f082ce36c688e.js'></script>
+<script src='<?php echo get_template_directory_uri() ?>/public/js/vendor.a6b6e5a92ed6f2d6999b.js'></script>
+<script src='<?php echo get_template_directory_uri() ?>/public/js/app.44854ee3c70d32245aa8.js'></script>
 <!--<script src="<?php echo get_template_directory_uri() ?>/public/js/lightbox.js"></script>-->
 
 <!--/app theme-->
