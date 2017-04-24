8f489f8b0a3496a23aa2935320982c2c1767ccc1
Julian Poveda <hjulianpoveda@gmail.com>
2017-04-24 17:06:45 -0500

    Margin modified

--- shortcodes/tri_news.php
+++ shortcodes/tri_news.php
@@ -45,6 +45,9 @@ function bs_tri_news_sc($atts, $content = null) {
       display:block!important;
     }
     @media (max-width: 760px){
+      .bs-post__recent{
+        margin:auto;
+        }
        .main_niw{
             width:100%; 
             float:right;
