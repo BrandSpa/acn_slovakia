d610ee7d320b100342a28139affd8efa0930b4cc
Julian Poveda <hjulianpoveda@gmail.com>
2017-04-29 00:37:11 -0500

    post type order changed

--- apis/posts.php
+++ apis/posts.php
@@ -2,7 +2,7 @@
 
 function bs_get_posts($type = 'post', $paged = 1, $category = '', $perpage = '6') {
 	$query = new Wp_Query(array(
-    'post_type' => array('post','video','gallery','featured'),//$type, Jul*
+    'post_type' => array('video','gallery','featured','post'),//$type, Jul*
     'paged' => $paged,
     'category_name' => $category,
 		'posts_per_page' => $perpage,
