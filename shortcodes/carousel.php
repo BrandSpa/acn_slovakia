e273dbfda7f81870741e5b0106b9f3c7ce16329c
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-09 16:12:24 -0500

    extend class

--- shortcodes/carousel.php
+++ shortcodes/carousel.php
@@ -47,7 +47,7 @@ function bs_carousel_sc($atts, $content = null) {
         "name" =>  "BS Carousel",
         "base" => "bs_carousel",
         "category" =>  "BS",
-				"as_parent" => array('only' => 'bs_arrow'), // Use only|except attributes to limit child shortcodes (separate multiple values with comma)
+				"as_parent" => array('only' => 'single_img'), // Use only|except attributes to limit child shortcodes (separate multiple values with comma)
 				"content_element" => true,
 				"show_settings_on_create" => false,
 				"is_container" => true,
@@ -58,3 +58,24 @@ function bs_carousel_sc($atts, $content = null) {
   }
 
   add_action( 'vc_before_init', 'bs_carousel_vc' );
+
+if ( class_exists( 'WPBakeryShortCodesContainer' ) ) {
+    class WPBakeryShortCode_bs_carousel extends WPBakeryShortCodesContainer {
+    }
+}
+
+	vc_map( array(
+    "name" => __("Gallery Image", "my-text-domain"),
+    "base" => "single_img",
+    "content_element" => true,
+    "as_child" => array('only' => 'your_gallery'), // Use only|except attributes to limit parent (separate multiple values with comma)
+    "params" => array(
+        // add params same as with any other content element
+        array(
+            "type" => "textfield",
+            "heading" => __("Extra class name", "my-text-domain"),
+            "param_name" => "el_class",
+            "description" => __("If you wish to style particular content element differently, then use this field to add a class name and then refer to it in your css file.", "my-text-domain")
+        )
+    )
+) );
