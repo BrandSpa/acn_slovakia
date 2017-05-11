a5b6e705c3a80d986a2e58230d5ffa42c1e00bff
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-11 16:41:58 -0500

    url-nea test

--- functions.php
+++ functions.php
@@ -241,6 +241,7 @@ function redirectToLang() {
 	
 	if(!isset($_COOKIE['bs-lang']) && empty($_COOKIE['bs-lang'])) {
 		$url = pll_the_languages( array( 'raw' => 1 ) )[$lang]['url'];
+		setcookie('url-nea', $url);
 		setcookie('bs-lang', $lang);
 		header('Location:'. $url);
 		exit;
