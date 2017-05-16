ec2694cf7f3bb77c709019641be87dce155e2931
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-16 10:56:06 -0500

    posts about to class

--- client/components/postsAbout.js
+++ client/components/postsAbout.js
@@ -1,54 +1,74 @@
-import React from 'react';
-import request from 'axios';
-import qs from 'qs';
-const endpoint = '/wp-admin/admin-ajax.php';
+import React from "react";
+import request from "axios";
+import qs from "qs";
+const endpoint = "/wp-admin/admin-ajax.php";
 
-const PostsAbout = React.createClass({
-	getInitialState() {
-		return {
-			posts: [],
-			loading: false
-		}
-	},
-	
-	componentWillReceiveProps(props) {
-		this.fetchPosts(props);
-	},
+class PostsAbout extends React.Component {
+  state = {
+    posts: [],
+    loading: false
+  };
 
-	componentDidMount() {
-		this.fetchPosts(this.props);
-	},
+  componentWillReceiveProps(props) {
+    this.fetchPosts(props);
+  }
 
-	fetchPosts(props) {
-		let data = qs.stringify({action: 'get_posts', 'post_perpage': 4, 'post_category': props.category });
-		this.setState({loading: true});
+  componentDidMount() {
+    this.fetchPosts(this.props);
+  }
 
-		request.post(endpoint, data)
-		.then(res => {
-        this.setState({loading: false, posts: res.data ? res.data : []});
+  fetchPosts = props => {
+    let data = qs.stringify({
+      action: "get_posts",
+      post_perpage: 4,
+      post_category: props.category
+    });
+    this.setState({ loading: true });
+
+    request
+      .post(endpoint, data)
+      .then(res => {
+        this.setState({ loading: false, posts: res.data ? res.data : [] });
       })
       .catch(err => console.error(err));
-	},
+  };
 
-	render() {
-		const { posts } = this.state;
-		return (
-			<div style={this.state.loading ? { transition: '300 ms', opacity: 0 } : { transition: '300 ms', opacity: 1 } }>
-			{posts.map((post, i) => {
-				return (
-					<div key={i} className="col-12 col-3-l" >
-						<a href={post.post_permalink} style={{background: '#fff'}}>
-							<img src={post.post_image} alt="" style={{width: '100%'}} />
-						</a>
-						<div className="post-about__title" style={{background: '#fff', padding: '20px', marginBottom: '20px'}}>
-							<h5><a style={{color: '#324049'}} href={post.post_permalink}>{post.post_title}</a></h5>
-						</div>
-					</div>
-				)
-			})}
-			</div>
-		)
-	}
-});
+  render() {
+    const { posts } = this.state;
+    return (
+      <div
+        style={
+          this.state.loading
+            ? { transition: "300 ms", opacity: 0 }
+            : { transition: "300 ms", opacity: 1 }
+        }
+      >
+        {posts.map((post, i) => {
+          return (
+            <div key={i} className="col-12 col-3-l">
+              <a href={post.post_permalink} style={{ background: "#fff" }}>
+                <img src={post.post_image} alt="" style={{ width: "100%" }} />
+              </a>
+              <div
+                className="post-about__title"
+                style={{
+                  background: "#fff",
+                  padding: "20px",
+                  marginBottom: "20px"
+                }}
+              >
+                <h5>
+                  <a style={{ color: "#324049" }} href={post.post_permalink}>
+                    {post.post_title}
+                  </a>
+                </h5>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default PostsAbout;
