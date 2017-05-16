298d93c8a5406408ac76132c5f1672d203249549
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-16 10:58:45 -0500

    section video to class

--- client/components/sectionVideo.js
+++ client/components/sectionVideo.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import VideoModal from './videoModal';
+import React from "react";
+import VideoModal from "./videoModal";
 
-const SectionVideo = React.createClass({
-  getDefaultProps() {
-    return {
-      imgUrl: '',
-      url: 'https://www.youtube.com/embed/_lQvw2vSDbs',
-      imageStyle: {}
-    };
-  },
+class SectionVideo extends React.Component {
+  static defaultProps = {
+    imgUrl: "",
+    url: "https://www.youtube.com/embed/_lQvw2vSDbs",
+    imageStyle: {}
+  };
 
-  showVideo(e) {
+  showVideo = e => {
     e.preventDefault();
     this.modal.show();
-  },
+  };
 
   render() {
-    let linkStyle = {float: 'left', lineHeight: '0'};
-    let imageStyle = {width: '100px', margin: '0 auto'};
-    imageStyle = {...imageStyle, ...this.props.imageStyle};
+    let linkStyle = { float: "left", lineHeight: "0" };
+    let imageStyle = { width: "100px", margin: "0 auto" };
+    imageStyle = { ...imageStyle, ...this.props.imageStyle };
 
     return (
       <div>
-        <VideoModal ref={modal => this.modal = modal} url={this.props.url} />
-        <a href="#" className="image-video__link" style={linkStyle} onClick={this.showVideo}>
+        <VideoModal ref={modal => (this.modal = modal)} url={this.props.url} />
+        <a
+          href="#"
+          className="image-video__link"
+          style={linkStyle}
+          onClick={this.showVideo}
+        >
           <img style={imageStyle} src={this.props.imgUrl} alt="" />
         </a>
       </div>
     );
   }
-});
+}
 
 export default SectionVideo;
--- client/components/sectionVideoContent.js
+++ client/components/sectionVideoContent.js
@@ -1,59 +1,63 @@
-import React from 'react';
-import VideoModal from './videoModal';
+import React from "react";
+import VideoModal from "./videoModal";
 import Radium, { StyleRoot } from "radium";
-const SectionVideoContent = React.createClass({
-  getDefaultProps() {
-    return {
-      imgUrl: '',
-      url: 'https://www.youtube.com/embed/_lQvw2vSDbs',
-      imageStyle: {},
-			content: '',
-      fullHeight: false
-    };
-  },
 
-  showVideo(e) {
+class SectionVideoContent extends React.Component {
+  static defaultProps = {
+    imgUrl: "",
+    url: "https://www.youtube.com/embed/_lQvw2vSDbs",
+    imageStyle: {},
+    content: "",
+    fullHeight: false
+  };
+
+  showVideo = e => {
     e.preventDefault();
     this.modal.show();
-  },
+  };
 
   render() {
     const h = window.innerHeight - 100;
     let linkStyle = {
-      left: '0',
-      right: '0',
-      bottom: '0',
-      top: '0',
-      width: '100%',
-      position: 'absolute'
+      left: "0",
+      right: "0",
+      bottom: "0",
+      top: "0",
+      width: "100%",
+      position: "absolute"
     };
 
     let bgStyle = {
-			float: 'left', 
-			lineHeight: '0',
-			background: `url(${this.props.imgUrl})`,
-			backgroundSize: 'cover',
-			backgroundPosition: 'center',
-			width: '100%',
-			height: this.props.fullHeight ?  h : '900px',
-			'@media (max-width: 767px)': {
-				height: 'auto'
-			}
-		};
+      float: "left",
+      lineHeight: "0",
+      background: `url(${this.props.imgUrl})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      width: "100%",
+      height: this.props.fullHeight ? h : "900px",
+      "@media (max-width: 767px)": {
+        height: "auto"
+      }
+    };
 
-    let imageStyle = {width: '100px', margin: '0 auto'};
-    imageStyle = {...imageStyle, ...this.props.imageStyle};
+    let imageStyle = { width: "100px", margin: "0 auto" };
+    imageStyle = { ...imageStyle, ...this.props.imageStyle };
 
     return (
       <StyleRoot>
-        <VideoModal ref={modal => this.modal = modal} url={this.props.url} />
+        <VideoModal ref={modal => (this.modal = modal)} url={this.props.url} />
         <div style={bgStyle}>
-        <a href="#" className="image-video__link" style={linkStyle} onClick={this.showVideo}></a>
-					<div dangerouslySetInnerHTML={{__html: this.props.content}} />
+          <a
+            href="#"
+            className="image-video__link"
+            style={linkStyle}
+            onClick={this.showVideo}
+          />
+          <div dangerouslySetInnerHTML={{ __html: this.props.content }} />
         </div>
       </StyleRoot>
     );
   }
-});
+}
 
 export default SectionVideoContent;
