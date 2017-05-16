f16bd0f9e947bf11eed95cc5dec42d33db232129
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-16 10:59:04 -0500

    video header to class

--- client/components/videoHeader.js
+++ client/components/videoHeader.js
@@ -1,48 +1,52 @@
-import React from 'react';
-import VideoModal from './videoModal';
+import React from "react";
+import VideoModal from "./videoModal";
 
-const VideoHeader = React.createClass({
-	getDefaultProps() {
-		return {
-			videoUrl: '',
-			imageUrl: ''
-		}
-	},
+class VideoHeader extends React.Component {
+  static defaultProps = {
+    videoUrl: "",
+    imageUrl: ""
+  };
 
-	openModal(e) {
-		e.preventDefault();
-		this.modal.show();
-	},
+  openModal = e => {
+    e.preventDefault();
+    this.modal.show();
+  };
 
-	render() {
-		const {videoUrl, imageUrl} = this.props;
-		let height = window.innerHeight < 700
-			? `${window.innerHeight / 2 - 100}px`
-			: `${window.innerHeight - 100}px`;
+  render() {
+    const { videoUrl, imageUrl } = this.props;
+    let height = window.innerHeight < 700
+      ? `${window.innerHeight / 2 - 100}px`
+      : `${window.innerHeight - 100}px`;
 
-		let style = {
-			backgroundImage: `url(${imageUrl})`,
-			backgroundSize: 'cover',
-			backgroundPosition: 'center',
-			width: '100%',
-			position: 'relative',
-			cursor: 'pointer',
-			height 
-		};
+    let style = {
+      backgroundImage: `url(${imageUrl})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      width: "100%",
+      position: "relative",
+      cursor: "pointer",
+      height
+    };
 
-		return (
-			<div>
-				<VideoModal ref={modal => this.modal = modal} url={videoUrl} />
-				<div style={style} onClick={this.openModal}>
-					<div style={{background: 'rgba(60,81,90, .5)', width: '100%', 'padding': '10px 10px'}}>
-						<div className="l-wrap" style={{color: '#fff', fontSize: '18px'}}>
-							Video <i className="ion-ios-videocam"></i> 
-						</div>
-					</div>
-				</div>
-			</div>
-		)
-	}
-});
+    return (
+      <div>
+        <VideoModal ref={modal => (this.modal = modal)} url={videoUrl} />
+        <div style={style} onClick={this.openModal}>
+          <div
+            style={{
+              background: "rgba(60,81,90, .5)",
+              width: "100%",
+              padding: "10px 10px"
+            }}
+          >
+            <div className="l-wrap" style={{ color: "#fff", fontSize: "18px" }}>
+              Video <i className="ion-ios-videocam" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default VideoHeader;
