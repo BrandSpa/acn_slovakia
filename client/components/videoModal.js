fc0ef4ee9175438b81c20936fa1d463ef55ad063
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-16 10:59:33 -0500

    video modal to class

--- client/components/videoModal.js
+++ client/components/videoModal.js
@@ -1,53 +1,55 @@
-import React from 'react';
+import React from "react";
 
-const Modal = React.createClass({
-  getInitialState() {
-    return {show: false};
-  },
+class Modal extends React.Component {
+  state = { show: false };
+
   componentDidMount() {
-    document.addEventListener('keydown', this.handleEscKey, false);
-  },
+    document.addEventListener("keydown", this.handleEscKey, false);
+  }
+
   componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleEscKey, false);
-  },
-  handleEscKey(e) {
-    if (e.keyCode == 27) this.setState({show: false});
-  },
-  close(e) {
+    document.removeEventListener("keydown", this.handleEscKey, false);
+  }
+
+  handleEscKey = e => {
+    if (e.keyCode == 27) this.setState({ show: false });
+  };
+
+  close = e => {
     e.preventDefault();
-    this.setState({show: false});
-  },
-  show() {
-    this.setState({show: true});
-  },
+    this.setState({ show: false });
+  };
+
+  show = () => {
+    this.setState({ show: true });
+  };
+
   render() {
-    let {url} = this.props;
-    
+    let { url } = this.props;
+
     let iframeStyle = {
-      height: window.innerHeight ? window.innerHeight : '100hv'
+      height: window.innerHeight ? window.innerHeight : "100hv"
     };
 
     return (
-      <div className={this.state.show ? 'modal modal--show' : 'modal'}>
+      <div className={this.state.show ? "modal modal--show" : "modal"}>
         <a className="modal__close" href="#" onClick={this.close}>
           <i className="ion-close" />
         </a>
         <div className="iframe-container" style={iframeStyle}>
-          {
-            this.state.show
-              ? <iframe
+          {this.state.show
+            ? <iframe
                 src={`${url}?autoplay=1`}
                 frameBorder="0"
                 height="315"
                 width="100%"
                 allowFullScreen
               />
-              : ''
-          }
+            : ""}
         </div>
       </div>
     );
   }
-});
+}
 
 export default Modal;
