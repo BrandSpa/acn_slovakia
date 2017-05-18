e0d5733779033b81404ab1b903476949de75da0a
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-18 16:00:29 -0500

    add smooth scroll

--- client/lib/smoothScroll.js
+++ client/lib/smoothScroll.js
@@ -2,20 +2,20 @@ import debounce from 'lodash/debounce';
 
 export default function smoothScroll() {
 
-  // $("a").on('click', function(event) {
+  $(".bs-donate").on('click', function(event) {
 
-  //   // Make sure this.hash has a value before overriding default behavior
-  //   if (this.hash !== "") {
-  //     event.preventDefault();
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      event.preventDefault();
 
-  //     const hash = this.hash;
-  //     const less = $('.nav') ? $('.nav').height() + 20 : 0;
+      const hash = this.hash;
+      const less = $('.nav') ? $('.nav').height() + 20 : 0;
 
-  //     const scrollTop = $(hash) ? ($(hash).offset().top - less) : 0;
-  //     $('html, body').animate({ scrollTop }, 800, () => {});
+      const scrollTop = $(hash) ? ($(hash).offset().top - less) : 0;
+      $('html, body').animate({ scrollTop }, 800, () => {});
 
-  //   }
-  // });
+    }
+  });
 
   window.addEventListener('scroll', debounce(function() {
      if($('.nav').offset().top > 0) {
--- footer.php
+++ footer.php
@@ -51,8 +51,8 @@
   console.log('<?php if(pll_home_url() == get_permalink()) echo 'nea' ?>');
 </script>
 
-<script src='<?php echo get_template_directory_uri() ?>/public/js/vendor.42b9a5fa87afc6f90262.js'></script>
-<script src='<?php echo get_template_directory_uri() ?>/public/js/app.f6201c6d6e18d5848d75.js'></script>
+<script src='<?php echo get_template_directory_uri() ?>/public/js/vendor.88b1ff9a4050e5785364.js'></script>
+<script src='<?php echo get_template_directory_uri() ?>/public/js/app.3f5c77cad6b8ab7bbd9b.js'></script>
 
 <!--/app theme-->
 
