95418235d92849adc6744900958f0395bd6b278b
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-19 11:22:34 -0500

    flame_top

--- shortcodes/candle.php
+++ shortcodes/candle.php
@@ -3,7 +3,9 @@ add_shortcode( 'bs_candle', 'bs_candle_sc' );
 
 function bs_candle_sc($atts, $content = null) {
 	$attributes = [
-		"width" => "150px"
+		"width" => "150px",
+		"style" => "margin: 80px auto -100px auto; position: relative",
+		"flame_top" => "-75px"
 	];
 
   $at = shortcode_atts( $attributes , $atts );
@@ -18,14 +20,14 @@ function bs_candle_sc($atts, $content = null) {
 }
 
 @keyframes show {
-  0%{ opacity: 0}
+  0%{ opacity: 0;}
   100%{ opacity: 1}
 }
 
 </style>
 
-<div class="candle-container" style="width: <?php echo $at['width'] ?>;margin: 80px auto -100px auto; position: relative">
-	<img style="width: 100%; position: absolute; top: -75px" src="<?php echo get_template_directory_uri() ?>/public/img/candle_flame.png" class="candle" alt="">
+<div class="candle-container" style="width: <?php echo $at['width'] ?>;<?php echo $at['style'] ?>">
+	<img style="width: 100%; position: absolute; top: <?php echo $at['flame_top'] ?>" src="<?php echo get_template_directory_uri() ?>/public/img/candle_flame.png" class="candle" alt="">
 	<img style="width: 100%" src="<?php echo get_template_directory_uri() ?>/public/img/candle_base.png" alt="">
 </div>
 
