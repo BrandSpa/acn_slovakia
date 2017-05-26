bcadde02f974f35620184f87939eb0348d05757b
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-26 10:22:43 -0500

    fix css name

--- client/app.js
+++ client/app.js
@@ -1,6 +1,7 @@
 'use strict';
 //get sass file and compiled to css
 import './scss/index.scss';
+
 import 'babel-polyfill';
 import WebFont from 'webfontloader';
 import multipleRender from 'react-multiple-render';
