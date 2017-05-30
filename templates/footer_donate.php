66240dbf7150d04155afc52cfaf77a8cf848a489
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-30 16:34:18 -0500

    fix class

--- templates/footer_donate.php
+++ templates/footer_donate.php
@@ -9,7 +9,7 @@
 				<p><?php echo gett('“I invite you all, together with ACN, to do everywhere in the world, a work of mercy.”'); ?></p>
 				<img width="122" height="23" src="//acninternational.org/wp-content/uploads/2017/02/SignPope.png" class="vc_single_image-img attachment-thumbnail" alt="signpope">
 		</div>
-		<div class="col-9-l bs-about__left">
+		<div class="col-9-l footer-donate__bottom--right">
 			<h3><?php echo gett('ABOUT US') ?></h3>
 			<p><?php echo gett('Founded in 1947 as a Catholic aid organization for war refugees and recognized as a papal foundation since 2011, ACN is dedicated to the service of Christians around the world, through information, prayer and action, wherever they are persecuted or oppressed or suffering material need. ACN supports every year an average of 6000 projects in close to 150 countries, thanks to private donations, as the foundation receives no public funding.') ?></p>
 		</div>
