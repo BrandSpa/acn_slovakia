64aaf1753485a8ff6d6f3f8ec77a4d0a2c9c0f35
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-31 14:00:35 -0500

    fixing footer responsive

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
 		<!-- /wordpress files-->
     
 
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/public/js/app.747c50e27ac8a730a27ccd948dadf1e8.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/public/js/app.38d7fdd474c5b0ed8c389cf4ede5e10c.css">
 	<link rel="stylesheet" href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   
 	<script>
