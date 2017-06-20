af635e2391a94d0aea8ab632044d51245e362ed0
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-20 10:30:20 -0500

    add types

--- apis/posts.php
+++ apis/posts.php
@@ -2,7 +2,7 @@
 
 function bs_get_posts($type = array('video','gallery','featured','post'), $paged = 1, $category = '', $perpage = '6') {
 	$query = new Wp_Query(array(
-    'post_type' => array('video','gallery','featured','post'),
+    'post_type' => $type,
     'paged' => $paged,
     'category_name' => $category,
 		'posts_per_page' => $perpage,
